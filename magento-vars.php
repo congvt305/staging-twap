de9e945577392394f05e8ce069c33b2022ed2c0e
soniapark <soniapark@eguanacommerce.com>
2020-06-02 08:03:33 +0900

    fix magento-vars for multi websites domain

--- magento-vars.php
+++ magento-vars.php
@@ -24,3 +24,32 @@ function isHttpHost(string $host)
     }
     return $_SERVER['HTTP_HOST'] === $host;
 }
+
+function getStoreCodeFromHost($serverHost)
+{
+    $storeCode = explode('.', $serverHost);
+    if (isset($storeCode[0]) && $storeCode[0]) {
+        return $storeCode[0];
+    }
+    return false;
+}
+
+$storeCodes = ['default', 'tw_laneige'];
+
+if (isHttpHost("tw.sulwhasoo.com")
+    || isHttpHost("mcprod.tw.sulwhasoo.com")
+    || isHttpHost("tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
+    || isHttpHost("mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
+    || isHttpHost("integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
+    || isHttpHost("mcstaging.tw.sulwhasoo.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "default";
+    $_SERVER["MAGE_RUN_TYPE"] = "store";
+} elseif (isHttpHost("tw.laneige.com")
+    || isHttpHost("mcprod.tw.laneige.com")
+    || isHttpHost("l.tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
+    || isHttpHost("l.mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
+    || isHttpHost("l.integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
+    || isHttpHost("mcstaging.tw.laneige.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "tw_laneige";
+    $_SERVER["MAGE_RUN_TYPE"] = "store";
+}
