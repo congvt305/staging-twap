c8d0086e5f8bc160c6aae1f8dbc159ac26c48bb6
Hyuna Ju <hyunaj@eguanacommerce.com>
2020-06-03 09:29:47 +0900

    Checkout design

---
+++ app/code/Eguana/CustomCheckout/Plugin/Checkout/Model/DefaultConfigProviderPlugin.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2020-06-02
+ * Time: 오후 7:19
+ */
+
+namespace Eguana\CustomCheckout\Plugin\Checkout\Model;
+
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Catalog\Model\ProductRepository as ProductRepository;
+
+class DefaultConfigProviderPlugin extends \Magento\Framework\Model\AbstractModel
+{
+    protected $checkoutSession;
+
+    protected $_productRepository;
+
+    /**
+     * DefaultConfigProviderPlugin constructor.
+     * @param CheckoutSession $checkoutSession
+     * @param ProductRepository $productRepository
+     */
+    public function __construct(
+        CheckoutSession $checkoutSession,
+        ProductRepository $productRepository
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->_productRepository = $productRepository;
+    }
+
+    /**
+     * @param \Magento\Checkout\Model\DefaultConfigProvider $subject
+     * @param array $result
+     * @return array
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function afterGetConfig(
+        \Magento\Checkout\Model\DefaultConfigProvider $subject,
+        array $result
+    ) {
+        $items = $result['totalsData']['items'];
+        foreach ($items as $index => $item) {
+            $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
+            $product = $this->_productRepository->getById($quoteItem->getProduct()->getId());
+            $result['quoteItemData'][$index]['laneige_size'] = $product->getAttributeText('laneige_size');
+        }
+        return $result;
+    }
+}
