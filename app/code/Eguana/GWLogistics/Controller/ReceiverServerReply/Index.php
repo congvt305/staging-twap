62b7fa79f5ef59b3768b51669b27e0cb00f300b0
soniapark <soniapark@eguanacommerce.com>
2020-06-07 18:38:11 +0900

    skip csrf validataion and close window

--- app/code/Eguana/GWLogistics/Controller/ReceiverServerReply/Index.php
+++ app/code/Eguana/GWLogistics/Controller/ReceiverServerReply/Index.php
@@ -11,10 +11,13 @@ namespace Eguana\GWLogistics\Controller\ReceiverServerReply;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\RawFactory;
 use Psr\Log\LoggerInterface;
 
-class Index extends Action
+class Index extends Action implements CsrfAwareActionInterface
 {
     /**
      * @var RawFactory
@@ -45,22 +48,41 @@ class Index extends Action
         $resultRaw = $this->rawFactory->create();
         $cvsStoreData = $this->getRequest()->getParams();
 
-//        if (!$cvsStoreData || $this->getRequest()->getMethod() !== 'POST' || !$this->getRequest()->isXmlHttpRequest()) {
         if (!$cvsStoreData || $this->getRequest()->getMethod() !== 'POST') {
             $this->logger->debug('isXmlHttpRequest: ', [$this->getRequest()->isXmlHttpRequest()]);
             return $resultRaw->setHttpResponseCode($httpBadRequestCode);
         }
-
+        $html = '';
         try {
             $this->saveCsvStoreData($cvsStoreData);
+            $html = '<script>window.close();</script>';
         } catch (\Exception $e) {
+            $html = $e->getMessage();
             $this->logger->error($e->getMessage());
         }
-        return $resultRaw->setHttpResponseCode($httpSuccessCode);
+        return $resultRaw->setContents($html);
     }
 
     private function saveCsvStoreData($cvsStoreData)
     {
         $this->logger->debug('cvsStoreData: ', $cvsStoreData);
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return InvalidRequestException|null
+     */
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
+    {
+        return null;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return bool|null
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return true;
     }
 }
