d944cea76274484a8d4a01fb19f223c1ae7a83b0
soniapark <soniapark@eguanacommerce.com>
2020-06-08 15:38:13 +0900

    change urls

--- magento-vars.php
+++ magento-vars.php
@@ -25,39 +25,39 @@ function isHttpHost(string $host)
     return $_SERVER['HTTP_HOST'] === $host;
 }
 
-function getStoreCodeFromHost($serverHost)
-{
-    $storeCode = explode('.', $serverHost);
-    if (isset($storeCode[0]) && $storeCode[0]) {
-        return $storeCode[0];
-    }
-    return false;
-}
-function isAdminBackendUri($uri)
-{
-    return (strpos($uri, '/admin') !== false);
-}
+//function getStoreCodeFromHost($serverHost)
+//{
+//    $storeCode = explode('.', $serverHost);
+//    if (isset($storeCode[0]) && $storeCode[0]) {
+//        return $storeCode[0];
+//    }
+//    return false;
+//}
+//function isAdminBackendUri($uri)
+//{
+//    return (strpos($uri, '/admin') !== false);
+//}
 
 $storeCodes = ['default', 'tw_laneige'];
 
 if (isHttpHost("tw.sulwhasoo.com")
     || isHttpHost("mcprod.tw.sulwhasoo.com")
-    || isHttpHost("tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
-    || isHttpHost("mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
-    || isHttpHost("integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
+//    || isHttpHost("tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
+//    || isHttpHost("mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
+    || isHttpHost("s.integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
     || isHttpHost("mcstaging.tw.sulwhasoo.com")) {
-    $_SERVER["MAGE_RUN_CODE"] = "tw_laneige";
+    $_SERVER["MAGE_RUN_CODE"] = "default";
     $_SERVER["MAGE_RUN_TYPE"] = "store";
 } elseif (isHttpHost("tw.laneige.com")
     || isHttpHost("mcprod.tw.laneige.com")
-    || isHttpHost("s.tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
-    || isHttpHost("s.mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
-    || isHttpHost("s.integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
+//    || isHttpHost("s.tw.laneige.com.c.kqevkj6gpg7si.ent.magento.cloud")
+//    || isHttpHost("s.mcstaging.tw.laneige.com.c.kqevkj6gpg7si.dev.ent.magento.cloud")
+    || isHttpHost("l.integration-5ojmyuq-kqevkj6gpg7si.ap-3.magentosite.cloud")
     || isHttpHost("mcstaging.tw.laneige.com")) {
-    $_SERVER["MAGE_RUN_CODE"] = "default";
+    $_SERVER["MAGE_RUN_CODE"] = "tw_laneige";
     $_SERVER["MAGE_RUN_TYPE"] = "store";
 }
-if (isAdminBackendUri($_SERVER['REQUEST_URI'])) {
-    $_SERVER["MAGE_RUN_CODE"] = "default";
-    $_SERVER["MAGE_RUN_TYPE"] = "store";
-}
+//if (isAdminBackendUri($_SERVER['REQUEST_URI'])) {
+//    $_SERVER["MAGE_RUN_CODE"] = "default";
+//    $_SERVER["MAGE_RUN_TYPE"] = "store";
+//}
