d5babf5de5a2543961eb0b2fa6f85809fd109865
soniapark <soniapark@eguanacommerce.com>
2020-06-16 14:35:56 +0900

    add city select in billing and shipping

--- app/code/Eguana/Directory/Model/ResourceModel/City.php
+++ app/code/Eguana/Directory/Model/ResourceModel/City.php
@@ -15,4 +15,32 @@ class City extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     {
         $this->_init('eguana_directory_region_city', 'city_id');
     }
+
+    public function loadByCode(\Eguana\Directory\Model\City $city, $cityCode, $regionId)
+    {
+        return $this->loadByRegion($city, $regionId, (string)$cityCode, 'code');
+    }
+
+    public function loadByRegion($object, $regionId, $value, $field)
+    {
+        $connection = $this->getConnection();
+        $select = $connection->select()->from(
+            ['city' => $this->getMainTable()]
+        )->where(
+            'city.region_id = ?',
+            $regionId
+        )->where(
+            "city.{$field} = ?",
+            $value
+        );
+
+        $data = $connection->fetchRow($select);
+        if ($data) {
+            $object->setData($data);
+        }
+
+        $this->_afterLoad($object);
+
+        return $this;
+    }
 }
