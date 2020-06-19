551f390f074ed0df6fdfd9926b14d21aed817dfe
Muhammad Umer Farooq <umerfarooq@eguanacommerce.com>
2020-06-19 13:12:27 +0500

    Eguana Social Login

---
+++ app/code/Eguana/SocialLogin/Controller/GoogleLogin/Callback.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * @author Eguana Team
+ * @copyright Copyright (c) 2020 Eguana {http://eguanacommerce.com}
+ * Created by PhpStorm
+ * User: umer
+ * Date: 4/6/20
+ * Time: 5:06 PM
+ */
+namespace Eguana\SocialLogin\Controller\GoogleLogin;
+
+use Eguana\SocialLogin\Helper\Data as Helper;
+use Eguana\SocialLogin\Model\SocialLoginHandler as SocialLoginModel;
+use Eguana\SocialLogin\Model\SocialLoginRepository;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface as ResultInterfaceAlias;
+use Magento\Framework\HTTP\Client\Curl;
+
+/**
+ * Class Callback
+ *
+ * Callback class for google login
+ */
+class Callback extends Action
+{
+    const SOCIAL_MEDIA_TYPE = 'google';
+    /**
+     * @var Helper
+     */
+    protected $helper;
+
+    /**
+     * @var Curl
+     */
+    private $curlClient;
+
+    /**
+     * @var SocialLoginModel
+     */
+    protected $socialLoginModel;
+
+    /**
+     * @var SocialLoginRepository
+     */
+    private $socialLoginRepository;
+
+    /**
+     * Callback constructor.
+     * @param Context $context
+     * @param Helper $helper
+     * @param Curl $curl
+     * @param SocialLoginModel $socialLoginModel
+     * @param SocialLoginRepository $socialLoginRepository
+     */
+    public function __construct(
+        Context $context,
+        Helper $helper,
+        Curl $curl,
+        SocialLoginModel $socialLoginModel,
+        SocialLoginRepository $socialLoginRepository
+    ) {
+        parent::__construct($context);
+        $this->helper                            = $helper;
+        $this->curlClient                        = $curl;
+        $this->socialLoginModel                  = $socialLoginModel;
+        $this->socialLoginRepository             = $socialLoginRepository;
+    }
+
+    /**
+     * Google login callback
+     * @return ResponseInterface|ResultInterfaceAlias|null
+     */
+    public function execute()
+    {
+        $socialMediaType = self::SOCIAL_MEDIA_TYPE;
+        $params = $this->getRequest()->getParams();
+        $clientId = $this->helper->getGoogleClientId();
+        $clientSecret = $this->helper->getGoogleClientSecret();
+        $clientRedirectUrl = $this->helper->getGoogleCallbackUrl();
+
+        // Google passes a parameter 'code' in the Redirect Url
+        if (isset($params['code'])) {
+            try {
+                // Get the access token
+                $data = $this->getAccessToken($clientId, $clientRedirectUrl, $clientSecret, $params['code']);
+
+                // Access Token
+                $access_token = $data['access_token'];
+
+                // Get user information
+                $user_info = $this->getUserProfileInfo($access_token);
+                $userid = $user_info['id'];
+            } catch (\Exception $e) {
+                $this->getResponse()->setBody(__($e->getMessage()));
+                return null;
+            }
+            $customerId = $this->socialLoginRepository->getSocialMediaCustomer($userid, $socialMediaType);
+            //If customer exists then login and close popup else close pop and redirect to social login page
+            $this->socialLoginModel->redirectCustomer($customerId, $user_info, $userid, $socialMediaType);
+            $this->helper->closePopUpWindow($this);
+        }
+    }
+
+    /**
+     * Holds the various APIs functions
+     * @param $client_id
+     * @param $redirect_uri
+     * @param $client_secret
+     * @param $code
+     * @return mixed
+     */
+    private function getAccessToken($client_id, $redirect_uri, $client_secret, $code)
+    {
+        $url = 'https://www.googleapis.com/oauth2/v4/token';
+        try {
+            $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=';
+            $curlPost = $curlPost . $client_secret . '&code=' . $code . '&grant_type=authorization_code';
+
+            $this->getCurlClient()->setOptions(
+                [
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_TIMEOUT => 60,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_POST => 1,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_POSTFIELDS => $curlPost
+                ]
+            );
+
+            $this->getCurlClient()->post($url, []);
+            $status = $this->getCurlClient()->getStatus();
+            if ($status != 200) {
+                $message = __('Unspecified OAuth error occurred.');
+                $this->getResponse()->setBody(__($message));
+                return null;
+            }
+            $response = json_decode($this->getCurlClient()->getBody(), true);
+            return $response;
+        } catch (\Exception $e) {
+            $this->getResponse()->setBody(__($e->getMessage()));
+            return null;
+        }
+    }
+
+    /**
+     * Get user profile info
+     * @param $access_token
+     * @return mixed
+     */
+    private function getUserProfileInfo($access_token)
+    {
+        $url = 'https://www.googleapis.com/oauth2/v2/userinfo?fields=name,email,gender,id,picture,verified_email';
+
+        try {
+            $this->getCurlClient()->setOptions(
+                [
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_TIMEOUT => 60,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_HTTPHEADER => [
+                    'Authorization: Bearer ' . $access_token,
+                    ]
+                ]
+            );
+
+            $this->getCurlClient()->get($url, []);
+            $status = $this->getCurlClient()->getStatus();
+            if ($status != 200) {
+                $message = __('Unspecified OAuth error occurred.');
+                $this->getResponse()->setBody(__($message));
+            }
+            $response = json_decode($this->getCurlClient()->getBody(), true);
+            return $response;
+        } catch (\Exception $e) {
+            $this->getResponse()->setBody(__($e->getMessage()));
+            return null;
+        }
+    }
+
+    /**
+     * @return Curl
+     */
+    private function getCurlClient()
+    {
+        return $this->curlClient;
+    }
+}
---
+++ app/code/Eguana/SocialLogin/Helper/Data.php
@@ -0,0 +1,217 @@
+<?php
+/**
+ * @author Eguana Team
+ * @copyriht Copyright (c) 2020 Eguana {http://eguanacommerce.com}
+ * Created by PhpStorm
+ * User: umer
+ * Date: 20/4/20
+ * Time: 12:42 PM
+ */
+namespace Eguana\SocialLogin\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper as AbstractHelperAlias;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\View\Element\Template as TemplateAlias;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Data
+ *
+ * Helper class
+ */
+class Data extends AbstractHelperAlias
+{
+    const XML_SOCIAL_LOGIN_ENABLE           = 'SocialLogin/general/sociallogin_mod_enable';
+    const XML_SOCIAL_LOGIN_LINE_ENABLE      = 'SocialLogin/line/line_enable';
+    const XML_SOCIAL_LINE_SECRET_ID         = 'SocialLogin/line/channel_id';
+    const XML_SOCIAL_LINE_CHANNEL_SECRET    = 'SocialLogin/line/channel_secret';
+    const XML_SOCIAL_LINE_CALLBACK_URL      = 'SocialLogin/line/callback_url';
+    const XML_SOCIAL_LOGIN_FACEBOOK_ENABLE  = 'SocialLogin/facebook/facebook_enable';
+    const XML_SOCIAL_FACEBOOK_APP_ID        = 'SocialLogin/facebook/app_id';
+    const XML_SOCIAL_FACEBOOK_APP_SECRET    = 'SocialLogin/facebook/app_secret';
+    const XML_SOCIAL_FACEBOOK_CALLBACK_URL  = 'SocialLogin/facebook/oauth_redirect_uri';
+    const XML_SOCIAL_GOOGLE_ENABLE          = 'SocialLogin/google/google_enable';
+    const XML_SOCIAL_GOOGLE_CLIENT_ID       = 'SocialLogin/google/client_id';
+    const XML_SOCIAL_GOOGLE_CLIENT_SECRET   = 'SocialLogin/google/client_secret';
+    const XML_SOCIAL_GOOGLE_CALLBACK_URL    = 'SocialLogin/google/oauth_redirect_uri';
+
+    /**
+     * @var PageFactory
+     */
+    private $resultPageFactory;
+
+    /**
+     * Data constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory                = $resultPageFactory;
+    }
+
+    /**
+     * Check if module is enabled or not
+     * @param null $store
+     * @return bool
+     */
+    public function isEnabledInFrontend($store = null)
+    {
+        $isEnabled = true;
+        $enabled =  $this->scopeConfig->getValue(self::XML_SOCIAL_LOGIN_ENABLE, ScopeInterface::SCOPE_STORE);
+        if ($enabled == null || $enabled == '0') {
+            $isEnabled = false;
+        }
+        return $isEnabled;
+    }
+
+    /**
+     * Close login pop up window
+     * @param $objectAction
+     */
+    public function closePopUpWindow($objectAction)
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $block = $resultPage->getLayout()
+            ->createBlock(TemplateAlias::class)
+            ->setTemplate('Eguana_SocialLogin::social/close-popup.phtml')
+            ->toHtml();
+        $objectAction->getResponse()->setBody($block);
+    }
+
+    /**
+     * Check if line login is enbled or not
+     * @param null $store
+     * @return bool
+     */
+    public function isEnabledLine($store = null)
+    {
+        $isEnabled = true;
+        $enabled =  $this->scopeConfig->getValue(
+            self::XML_SOCIAL_LOGIN_LINE_ENABLE,
+            ScopeInterface::SCOPE_STORE
+        );
+        if ($enabled == null || $enabled == '0') {
+            $isEnabled = false;
+        }
+        return $isEnabled;
+    }
+
+    /**
+     * Check if facebook login is enabled or not
+     * @param null $store
+     * @return bool
+     */
+    public function isEnabledFacebook($store = null)
+    {
+        $isEnabled = true;
+        $enabled =  $this->scopeConfig->getValue(
+            self::XML_SOCIAL_LOGIN_FACEBOOK_ENABLE,
+            ScopeInterface::SCOPE_STORE
+        );
+        if ($enabled == null || $enabled == '0') {
+            $isEnabled = false;
+        }
+        return $isEnabled;
+    }
+
+    /**
+     * Check if google login is enabled or not
+     * @param null $store
+     * @return bool
+     */
+    public function isEnabledGoogle($store = null)
+    {
+        $isEnabled = true;
+        $enabled =  $this->scopeConfig->getValue(self::XML_SOCIAL_GOOGLE_ENABLE, ScopeInterface::SCOPE_STORE);
+        if ($enabled == null || $enabled == '0') {
+            $isEnabled = false;
+        }
+        return $isEnabled;
+    }
+
+    /**
+     * Get secret id
+     * @return string
+     */
+    public function getSecretId()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_LINE_SECRET_ID, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get app id
+     * @return string
+     */
+    public function getAppId()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_FACEBOOK_APP_ID, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get channel secret
+     * @return string
+     */
+    public function getChannelSecret()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_LINE_CHANNEL_SECRET, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get app secret
+     * @return string
+     */
+    public function getAppSecret()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_FACEBOOK_APP_SECRET, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get callback url
+     * @return string
+     */
+    public function getCallbackUrl()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_LINE_CALLBACK_URL, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get fb callback url
+     * @return string
+     */
+    public function getFbCallbackUrl()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_FACEBOOK_CALLBACK_URL, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get google client id
+     * @return string
+     */
+    public function getGoogleClientId()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_GOOGLE_CLIENT_ID, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get google client secret
+     * @return string
+     */
+    public function getGoogleClientSecret()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_GOOGLE_CLIENT_SECRET, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get google callback url
+     * @return string
+     */
+    public function getGoogleCallbackUrl()
+    {
+        return $this->scopeConfig->getValue(self::XML_SOCIAL_GOOGLE_CALLBACK_URL, ScopeInterface::SCOPE_STORE);
+    }
+}
