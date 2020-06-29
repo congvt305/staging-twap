254b069bb2818ac1210355343cecfda6f6cf2a66
brian <brianchang@eguanacommerce.com>
2020-06-29 11:25:15 +0900

    change validation when order sending

--- app/code/Amore/Sap/Controller/Adminhtml/SapOrder/OrderSend.php
+++ app/code/Amore/Sap/Controller/Adminhtml/SapOrder/OrderSend.php
@@ -10,10 +10,10 @@ namespace Amore\Sap\Controller\Adminhtml\SapOrder;
 
 use Amore\Sap\Model\Connection\Request;
 use Amore\Sap\Model\SapOrder\SapOrderConfirmData;
+use Amore\Sap\Model\Source\Config;
 use Ecpay\Ecpaypayment\Model\Order;
 use Magento\Backend\App\Action;
 use Magento\Backend\Model\View\Result\Redirect;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
@@ -38,6 +38,10 @@ class OrderSend extends Action
      * @var OrderRepositoryInterface
      */
     private $orderRepository;
+    /**
+     * @var Config
+     */
+    private $config;
 
     /**
      * OrderSend constructor.
@@ -46,49 +50,71 @@ class OrderSend extends Action
      * @param SapOrderConfirmData $sapOrderConfirmData
      * @param Request $request
      * @param OrderRepositoryInterface $orderRepository
+     * @param Config $config
      */
     public function __construct(
         Action\Context $context,
         Json $json,
         SapOrderConfirmData $sapOrderConfirmData,
         Request $request,
-        OrderRepositoryInterface $orderRepository
+        OrderRepositoryInterface $orderRepository,
+        Config $config
     ) {
         parent::__construct($context);
         $this->json = $json;
         $this->sapOrderConfirmData = $sapOrderConfirmData;
         $this->request = $request;
         $this->orderRepository = $orderRepository;
+        $this->config = $config;
     }
 
     public function execute()
     {
-        $orderId = $this->getRequest()->getParam('id');
-        $order = $this->orderRepository->get($orderId);
+        if (!$this->config->checkTestMode()) {
+            $orderId = $this->getRequest()->getParam('id');
+            $order = $this->orderRepository->get($orderId);
 
-        try {
-            $orderSendData = $this->sapOrderConfirmData->singleOrderData($order->getIncrementId());
-            $result = $this->request->postRequest($this->json->serialize($orderSendData), $order->getStoreId());
+            try {
+                $orderSendData = $this->sapOrderConfirmData->singleOrderData($order->getIncrementId());
+                $result = $this->request->postRequest($this->json->serialize($orderSendData), $order->getStoreId());
 
+                $resultSize = count($result);
 
-            $resultSize = count($result);
-//            $resultSize = 1;
+                if ($resultSize > 0) {
+                    $this->messageManager->addSuccessMessage(__('Order %1 sent to SAP Successfully.', $order->getIncrementId()));
+                } else {
+                    $this->messageManager->addErrorMessage(__('Something went wrong while sending order data to SAP. No response'));
+                }
 
-            if ($resultSize > 0) {
-                $this->messageManager->addSuccessMessage(__('Order %1 sent to SAP Successfully.', $order->getIncrementId()));
-            } else {
-                $this->messageManager->addErrorMessage(__('Something went wrong while sending order data to SAP.'));
+            } catch (NoSuchEntityException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+            } catch (LocalizedException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
             }
+        } else {
+            try {
+                $testOrderData = $this->sapOrderConfirmData->getTestOrderConfirm();
 
-        } catch (NoSuchEntityException $e) {
-            $this->messageManager->addErrorMessage($e->getMessage());
-        } catch (LocalizedException $e) {
-            $this->messageManager->addErrorMessage($e->getMessage());
+                $serializedTestData = $this->json->serialize($testOrderData);
+                $result = $this->request->postRequest($serializedTestData, 0, 'confirm');
+
+                $resultSize = count($result);
+                if ($resultSize > 0) {
+                    $this->messageManager->addSuccessMessage(__('Test Order sent to SAP Successfully.'));
+                } else {
+                    $this->messageManager->addErrorMessage(__('Something went wrong while sending Test order data to SAP. No response'));
+                }
+
+            } catch (\Exception $exception) {
+                $this->messageManager->addErrorMessage($exception->getMessage());
+            }
         }
 
         /** @var Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        return $resultRedirect->setPath('*/*/index');
+        $resultRedirect->setPath('sales/order/index');
+
+        return $resultRedirect;
     }
 
     protected function _isAllowed()
