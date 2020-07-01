37badd14c28a70281daba889ba179a01e77d1ca3
bangji <bangji@eguanacommerce.com>
2020-07-01 20:39:41 +0900

    Create a Base module for Eguana Extensions

---
+++ app/code/Eguana/Base/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Eguana Commerce
+ * @copyright Copyright (c) 2020 Eguana Abbas
+ * @package Eguana_Base
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Eguana_Base',
+    __DIR__
+);
