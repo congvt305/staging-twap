6b930f7dd2d07b47fa79a9f0b638068882274e69
arslan <arslaniqbal@eguanacommerce.com>
2020-07-07 11:44:10 +0500

    Eguana VideoBoard Mass Action Added and ACL fixed

---
+++ app/code/Eguana/VideoBoard/Controller/Adminhtml/HowTo/MassDelete.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @author Eguana Team
+ * @copyriht Copyright (c) 2020 Eguana {http://eguanacommerce.com}
+ * Created by PhpStorm
+ * User: arslan
+ * Date: 3/7/20
+ * Time: 1:11 PM
+ */
+namespace Eguana\VideoBoard\Controller\Adminhtml\HowTo;
+
+use Eguana\VideoBoard\Model\VideoBoard;
+use Eguana\VideoBoard\Model\ResourceModel\VideoBoard\CollectionFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\View\Result\Redirect;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Ui\Component\MassAction\Filter;
+
+/**
+ * This class is used for Mass delete
+ * Class MassDelete
+ */
+class MassDelete extends Action
+{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute action
+     *
+     * @return Redirect
+     * @throws LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        /**
+         * @var Video $video
+         */
+        foreach ($collection as $video) {
+            $video->delete();
+        }
+
+        $this->messageManager->addSuccessMessage(__('A total of %1 video(s) have been deleted.', $collectionSize));
+
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
