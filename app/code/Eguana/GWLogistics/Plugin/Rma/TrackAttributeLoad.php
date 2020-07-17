3be4eea46d9aaaf78e7ee9bb31fbed0d32d49e32
soniapark <soniapark@eguanacommerce.com>
2020-07-17 10:55:04 +0900

    add plugin

--- app/code/Eguana/GWLogistics/Plugin/Rma/TrackAttributeLoad.php
+++ app/code/Eguana/GWLogistics/Plugin/Rma/TrackAttributeLoad.php
@@ -8,8 +8,18 @@
 
 namespace Eguana\GWLogistics\Plugin\Rma;
 
+use Magento\Rma\Api\Data\TrackInterface;
 
 class TrackAttributeLoad
 {
+    /**
+     * @param \Magento\Rma\Api\Data\TrackInterface $subject
+     * @param $result
+     */
+    public function afterGetExtensionAttributes(\Magento\Rma\Api\Data\TrackInterface $subject, $result)
+    {
+        // TODO: Implement plugin method.
+        return $result;
+    }
 
 }
