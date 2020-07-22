f339cefa7df4fabfa813a4cbc840eb9bf90abed6
Sonia Park <soniapark@eguanacommerce.com>
2020-07-22 02:38:30 +0000

    APT-149

---
+++ app/code/Eguana/CustomRMA/Plugin/ReturnsSubmitPlugin.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by Eguana Team.
+ * User: sonia
+ * Date: 7/22/20
+ * Time: 8:56 AM
+ */
+
+namespace Eguana\CustomRMA\Plugin;
+
+use Magento\Framework\Exception\AlreadyExistsException;
+
+class ReturnsSubmitPlugin
+{
+    /**
+     * @var \Magento\Rma\Model\ResourceModel\Rma
+     */
+    private $rmaResource;
+    /**
+     * @var \Magento\Rma\Model\RmaFactory
+     */
+    private $rmaFactory;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        \Magento\Rma\Model\ResourceModel\Rma $rmaResource,
+        \Magento\Rma\Model\RmaFactory $rmaFactory,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->rmaResource = $rmaResource;
+        $this->rmaFactory = $rmaFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param \Magento\Rma\Controller\Returns\Submit $subject
+     * @param $result
+     */
+    public function afterExecute(\Magento\Rma\Controller\Returns\Submit $subject, $result)
+    {
+        $shippingPreference = $subject->getRequest()->getParam('shipping_preference');
+        if ($shippingPreference) {
+            $orderId = (int)$subject->getRequest()->getParam('order_id');
+            try {
+                $rmaModel = $this->rmaFactory->create();
+                $this->rmaResource->load($rmaModel, $orderId, 'order_id');
+                $rmaModel->setData('shipping_preference', $shippingPreference);
+                $this->rmaResource->save($rmaModel);
+            } catch (AlreadyExistsException $e) {
+                $this->logger->critical($e->getMessage());
+            }
+        }
+        return $result;
+    }
+}
