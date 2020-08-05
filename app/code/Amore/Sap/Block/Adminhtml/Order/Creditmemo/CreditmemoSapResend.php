b5e6bae12196509d3fc8f274dd7ffb631a2fa8fa
brian <brianchang@eguanacommerce.com>
2020-08-05 10:24:30 +0900

    change condition to show sap resend btn

--- app/code/Amore/Sap/Block/Adminhtml/Order/Creditmemo/CreditmemoSapResend.php
+++ app/code/Amore/Sap/Block/Adminhtml/Order/Creditmemo/CreditmemoSapResend.php
@@ -129,9 +129,11 @@ class CreditmemoSapResend extends \Magento\Sales\Block\Adminhtml\Order\Creditmem
             );
         }
 
-        if ($this->config->getActiveCheck('store', $this->getCreditmemo()->getStoreId()) && !$this->config->checkTestMode()) {
+        if ($this->config->getActiveCheck('store', $this->getCreditmemo()->getStoreId()) &&
+            !$this->config->checkTestMode() &&
+            $this->config->getCreditmemoActiveCheck('store', $this->getCreditmemo()->getStoreId())) {
             $creditmemoSendCheck = $this->getCreditmemo()->getOrder()->getData('sap_creditmemo_send_check');
-            if ($creditmemoSendCheck == 0 || $creditmemoSendCheck == 2) {
+            if (!is_null($creditmemoSendCheck) && ($creditmemoSendCheck == 0 || $creditmemoSendCheck == 2)) {
                 $this->buttonList->add(
                     'sap_refund_send',
                     [
