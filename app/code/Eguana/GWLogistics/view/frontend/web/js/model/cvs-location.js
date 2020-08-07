4e7dcec6e7dd636b456df0f0723a6584e330222a
soniapark <soniapark@eguanacommerce.com>
2020-08-07 10:27:32 +0900

    remove redundant cvs sotre field in checkout

--- app/code/Eguana/GWLogistics/view/frontend/web/js/model/cvs-location.js
+++ app/code/Eguana/GWLogistics/view/frontend/web/js/model/cvs-location.js
@@ -14,27 +14,6 @@ define([
             return sectionData()['cvs-location'] || [];
         },
 
-        getMessage: function () {
-            var location = this.getCvsLocation();
-            var searchCvsRequest = this.getSearchCvsRequest();
-            var locationsCount = _.size(location);
-
-            if (_.isEmpty(searchCvsRequest)) {
-                return $.mage.__('Please wait.');
-            } else if (_.isEmpty(location) && _.size(searchCvsRequest) > 0) {
-                return $.mage.__('No cvs location found.');
-            } else {
-                return $.mage.__('There were %1 results for your search.').replace('%1', locationsCount);
-            }
-        },
-
-        getSearchCvsRequest: function () {
-            if (_.size(sectionData()['search-cvs-request']) > 0) {
-                return sectionData()['search-cvs-request'];
-            }
-            return false;
-        },
-
         selectCvsLocation: function () {
             this.reloadCheckoutData();
         },
@@ -56,7 +35,6 @@ define([
         clear: function() {
             customerData.set(cacheKey, {
                 'cvs-location': [],
-                'search-request': {}
             });
         }
     };
