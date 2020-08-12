486de39763108954dc13e873b18d0a1f7354f2d2
brian <brianchang@eguanacommerce.com>
2020-08-12 16:01:22 +0900

    add custom exception and change to save sap_sent_check

---
+++ app/code/Amore/Sap/Exception/CrditmemoException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Eguana.
+ * User: Brian
+ * Date: 2020-08-12
+ * Time: 오후 3:20
+ */
+
+namespace Amore\Sap\Exception;
+
+class CrditmemoException extends \Magento\Framework\Exception\LocalizedException
+{
+
+}
