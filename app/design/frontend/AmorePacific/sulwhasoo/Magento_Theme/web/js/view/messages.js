60c2131fe65e63510ae16c9564918c792dbb26e2
Hyuna Ju <hyunaj@eguanacommerce.com>
2020-08-28 11:12:50 +0900

    message js update

--- app/design/frontend/AmorePacific/sulwhasoo/Magento_Theme/web/js/view/messages.js
+++ app/design/frontend/AmorePacific/sulwhasoo/Magento_Theme/web/js/view/messages.js
@@ -11,8 +11,9 @@ define([
     'uiComponent',
     'Magento_Customer/js/customer-data',
     'underscore',
+    'escaper',
     'jquery/jquery-storageapi'
-], function ($, Component, customerData, _) {
+], function ($, Component, customerData, _, escaper) {
     'use strict';
 
     return Component.extend({
@@ -20,7 +21,8 @@ define([
             cookieMessages: [],
             messages: [],
             selector: '.page.messages .message',
-            removeButton: '.page.messages .message-visible .remove'
+            removeButton: '.page.messages .message-visible .remove',
+            allowedTags: ['div', 'span', 'b', 'strong', 'i', 'em', 'u', 'a']
         },
 
         /**
@@ -37,6 +39,7 @@ define([
             var removeButton = this.removeButton;
             $(removeButton).show();
 
+            // Force to clean obsolete messages
             if (!_.isEmpty(this.messages().messages)) {
                 customerData.set('messages', {});
             }
@@ -47,5 +50,15 @@ define([
                 $(event.target).closest('div.message').slideUp();
             });
         },
+
+        /**
+         * Prepare the given message to be rendered as HTML
+         *
+         * @param {String} message
+         * @return {String}
+         */
+        prepareMessageForHtml: function (message) {
+            return escaper.escapeHtml(message, this.allowedTags);
+        }
     });
 });
