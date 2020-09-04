edc433a21fc07c9736fe7ebdcb7506b468346d47
soniapark <soniapark@eguanacommerce.com>
2020-09-04 11:02:57 +0900

    add buy now

--- app/code/Amore/GaTagging/view/frontend/web/js/action/notify.js
+++ app/code/Amore/GaTagging/view/frontend/web/js/action/notify.js
@@ -4,6 +4,7 @@ define([], function () {
     function notify(eventName) {
         window.dataLayer.push({'event': eventName});
     }
+
     return function (config, element) {
         if (window.dataLayer) {
             notify(config.eventName);
