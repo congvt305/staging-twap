a8e107ee2ae6aa71c7439cf4948d258eb8e5c93c
soniapark <soniapark@eguanacommerce.com>
2020-09-04 13:38:16 +0900

    add breadcrumbs

--- app/code/Amore/GaTagging/Plugin/BreadCrumbsPlugin.php
+++ app/code/Amore/GaTagging/Plugin/BreadCrumbsPlugin.php
@@ -13,6 +13,22 @@ use Magento\Theme\Block\Html\Breadcrumbs;
 
 class BreadCrumbsPlugin
 {
+    /**
+     * @var \Magento\Framework\Registry
+     */
+    private $registry;
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    private $request;
+
+    public function __construct(
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\RequestInterface $request
+    ) {
+        $this->registry = $registry;
+        $this->request = $request;
+    }
 
     /**
      * @param \Magento\Theme\Block\Html\Breadcrumbs $subject
@@ -21,12 +37,37 @@ class BreadCrumbsPlugin
     public function afterToHtml(\Magento\Theme\Block\Html\Breadcrumbs $subject, $result)
     {
         $breadCrumbText = "";
-        if ($result) {
-            $breadCrumbText = str_replace('</li>', '>', $result);
-            $breadCrumbText = strip_tags($breadCrumbText);
-            $breadCrumbText = str_replace(["\n", ' '], '', $breadCrumbText);
-            $breadCrumbText = substr($breadCrumbText, 0, -1);
+        if (!$result) {
+            return $this->makeScript($breadCrumbText, $result);
         }
+
+        if ($this->registry->registry('product')) {
+            $breadCrumbText = $this->getProductPageBreadCrumbs();
+            return $this->makeScript($breadCrumbText, $result);
+        }
+
+        $breadCrumbText = str_replace('</li>', '>', $result);
+        $breadCrumbText = strip_tags($breadCrumbText);
+        $breadCrumbText = str_replace(["\n", ' '], '', $breadCrumbText);
+        $breadCrumbText = substr($breadCrumbText, 0, -1);
+
+        return $this->makeScript($breadCrumbText, $result);
+    }
+    private function getProductPageBreadCrumbs()
+    {
+        /** @var \Magento\Catalog\Model\Product $product */
+        $product = $this->registry->registry('product');
+        /** @var \Magento\Catalog\Model\Category $category */
+        $category = $product->getCategory();
+        $breadCrumbText = __('Home') . '>';
+        if ($category) {
+            $breadCrumbText .= $category->getName() . '>';
+        }
+        $breadCrumbText .= $product->getName();
+        return $breadCrumbText;
+    }
+    private function makeScript($breadCrumbText, $result)
+    {
         $apScript = '<script>var AP_DATA_BREAD = ' . '"' . $breadCrumbText . '"'.'; </script>';
         return $result . $apScript;
     }
--- app/code/Amore/GaTagging/view/frontend/web/js/action/notify.js
+++ app/code/Amore/GaTagging/view/frontend/web/js/action/notify.js
@@ -2,6 +2,7 @@ define([], function () {
     'use strict';
 
     function notify(eventName) {
+        console.log('dataLayer push ', eventName);
         window.dataLayer.push({'event': eventName});
     }
 
