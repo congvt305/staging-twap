afbee59e4c1f5a9efaa9cc8a6306ef4008e07040
Muhammad Umer Farooq <umerfarooq@eguanacommerce.com>
2020-09-24 19:41:19 +0500

    configurable outof stock products price

--- app/code/Eguana/CustomCatalog/view/frontend/requirejs-config.js
+++ app/code/Eguana/CustomCatalog/view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@
  * @author Eguana Team
  * @copyright Copyright (c) 2019 Eguana {http://eguanacommerce.com}
  * Created by PhpStorm
- * User: arslan
+ * User: umer
  * Date: 27/8/20
  * Time: 7:24 PM
  */
@@ -11,6 +11,10 @@ var config = {
         'mixins': {
             'Magento_Wishlist/js/add-to-wishlist': {
                 'Eguana_CustomCatalog/js/add-to-wishlist-mixins': true
+            },
+            "Magento_Swatches/js/swatch-renderer" : {
+                "Eguana_CustomCatalog/js/swatch-renderer-mixin": true
+
             }
         }
     }
---
+++ app/code/Eguana/CustomCatalog/view/frontend/web/js/swatch-renderer-mixin.js
@@ -0,0 +1,162 @@
+/**
+ * @author Eguana Team
+ * @copyright Copyright (c) 2020 Eguana {http://eguanacommerce.com}
+ * Created by PhpStorm
+ * User: umer
+ * Date: 22/9/20
+ * Time: 6:54 PM
+ */
+define([
+    'jquery',
+    'underscore',
+    'jquery/ui',
+    'jquery/jquery.parsequery',
+    'mage/translate'
+], function ($, _) {
+    'use strict';
+    return function (widget) {
+
+        $.widget('mage.SwatchRenderer', widget, {
+
+            /**
+             * Event for swatch options
+             *
+             * @param {Object} $this
+             * @param {Object} $widget
+             * @private
+             */
+            _OnClick: function ($this, $widget) {
+                $widget._super($this, $widget);
+                if ($this.hasClass('outofstock')) {
+                    $('button[id="product-addtocart-button"]').attr('disabled', true);
+                    $('button[id="product-addtocheckout-button"]').attr('disabled', true);
+                } else {
+                    $('button[id="product-addtocart-button"]').attr('disabled', false);
+                    $('button[id="product-addtocheckout-button"]').attr('disabled', false);
+                }
+            },
+
+            /**
+             * Render swatch options by part of config
+             *
+             * @param {Object} config
+             * @param {String} controlId
+             * @returns {String}
+             * @private
+             */
+            _RenderSwatchOptions: function (config, controlId) {
+                var optionConfig = this.options.jsonSwatchConfig[config.id],
+                    optionClass = this.options.classes.optionClass,
+                    sizeConfig = this.options.jsonSwatchImageSizeConfig,
+                    moreLimit = parseInt(this.options.numberToShow, 10),
+                    moreClass = this.options.classes.moreButton,
+                    moreText = this.options.moreButtonText,
+                    countAttributes = 0,
+                    html = '';
+
+                if (!this.options.jsonSwatchConfig.hasOwnProperty(config.id)) {
+                    return '';
+                }
+
+                $.each(config.options, function (index) {
+                    var id,
+                        type,
+                        value,
+                        thumb,
+                        label,
+                        width,
+                        height,
+                        attr,
+                        swatchImageWidth,
+                        swatchImageHeight;
+
+                    if (!optionConfig.hasOwnProperty(this.id)) {
+                        return '';
+                    }
+
+                    // Add more button
+                    if (moreLimit === countAttributes++) {
+                        html += '<a href="#" class="' + moreClass + '"><span>' + moreText + '</span></a>';
+                    }
+
+                    id = this.id;
+                    type = parseInt(optionConfig[id].type, 10);
+                    value = optionConfig[id].hasOwnProperty('value') ?
+                        $('<i></i>').text(optionConfig[id].value).html() : '';
+                    thumb = optionConfig[id].hasOwnProperty('thumb') ? optionConfig[id].thumb : '';
+                    width = _.has(sizeConfig, 'swatchThumb') ? sizeConfig.swatchThumb.width : 110;
+                    height = _.has(sizeConfig, 'swatchThumb') ? sizeConfig.swatchThumb.height : 90;
+                    label = this.label ? $('<i></i>').text(this.label).html() : '';
+                    attr =
+                        ' id="' + controlId + '-item-' + id + '"' +
+                        ' index="' + index + '"' +
+                        ' aria-checked="false"' +
+                        ' aria-describedby="' + controlId + '"' +
+                        ' tabindex="0"' +
+                        ' option-type="' + type + '"' +
+                        ' option-id="' + id + '"' +
+                        ' option-label="' + label + '"' +
+                        ' aria-label="' + label + '"' +
+                        ' option-tooltip-thumb="' + thumb + '"' +
+                        ' option-tooltip-value="' + value + '"' +
+                        ' role="option"' +
+                        ' thumb-width="' + width + '"' +
+                        ' thumb-height="' + height + '"';
+
+                    swatchImageWidth = _.has(sizeConfig, 'swatchImage') ? sizeConfig.swatchImage.width : 30;
+                    swatchImageHeight = _.has(sizeConfig, 'swatchImage') ? sizeConfig.swatchImage.height : 20;
+
+                    if (!this.hasOwnProperty('products') || this.products.length <= 0) {
+                        attr += ' option-empty="true"';
+                    }
+                    if (this.stockStatus == 0) {
+                        attr += ' option-stock="false"';
+                    }
+                    if (type === 0) {
+                        // Text
+                        html += '<div class="' + optionClass + ' text" ' + attr + '>' + (value ? value : label) +
+                            '</div>';
+                    } else if (type === 1) {
+                        // Color
+                        html += '<div class="' + optionClass + ' color" ' + attr +
+                            ' style="background: ' + value +
+                            ' no-repeat center; background-size: initial;">' + '' +
+                            '</div>';
+                    } else if (type === 2) {
+                        // Image
+                        html += '<div class="' + optionClass + ' image" ' + attr +
+                            ' style="background: url(' + value + ') no-repeat center; background-size: initial;width:' +
+                            swatchImageWidth + 'px; height:' + swatchImageHeight + 'px">' + '' +
+                            '</div>';
+                    } else if (type === 3) {
+                        // Clear
+                        html += '<div class="' + optionClass + '" ' + attr + '></div>';
+                    } else {
+                        // Default
+                        html += '<div class="' + optionClass + '" ' + attr + '>' + label + '</div>';
+                    }
+                });
+
+                return html;
+            },
+
+            /**
+             * Rewind options for controls
+             *
+             * @private
+             */
+            _Rewind: function (controls) {
+                controls.find('div[option-id], option[option-id]').removeClass('disabled').removeAttr('disabled');
+                controls.find('div[option-empty], option[option-empty]')
+                    .attr('disabled', true)
+                    .addClass('disabled')
+                    .attr('tabindex', '-1');
+                controls.find('div[option-stock]')
+                    .addClass('outofstock');
+            }
+
+        });
+
+        return $.mage.SwatchRenderer;
+    }
+});
