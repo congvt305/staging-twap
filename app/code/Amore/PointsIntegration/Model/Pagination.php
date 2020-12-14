02cd1978a2a5a7b923778222611b5a2730459f44
David Kim <davidkim@eguanacommerce.com>
2020-12-14 15:15:33 +0900

    Modified variable name and class name

--- app/code/Amore/PointsIntegration/Model/Pagination.php
+++ app/code/Amore/PointsIntegration/Model/Pagination.php
@@ -13,21 +13,21 @@ namespace Amore\PointsIntegration\Model;
 class Pagination
 {
     /**
-     * @param array $redeemPointsResult
+     * @param array $pageData
      * @return array
      */
-    public function ajaxPagination(array $redeemPointsResult): array
+    public function ajaxPagination(array $pageData): array
     {
         //Total number of page ranges
-        $pageCnt = $redeemPointsResult[0]['totPage'];
+        $pageCnt = $pageData[0]['totPage'];
         //Number of posts per page
-        $listSize = count($redeemPointsResult);
+        $listSize = count($pageData);
         //Total number of posts
-        $listCnt = $redeemPointsResult[0]['totCnt'];
+        $listCnt = $pageData[0]['totCnt'];
         //Number of pages to show
         $rangeSize = 5;
         //Current page
-        $page = $redeemPointsResult[0]['page'];
+        $page = $pageData[0]['page'];
         //Current page range
         $range = ceil($page / $rangeSize);
         //Start page
@@ -39,17 +39,17 @@ class Pagination
         //Next page
         $next = $endPage > $pageCnt ? false : true;
 
-        $redeemPointsResult[0]['pageCnt'] = $pageCnt;
-        $redeemPointsResult[0]['listSize'] = $listSize;
-        $redeemPointsResult[0]['listCnt'] = $listCnt;
-        $redeemPointsResult[0]['rangeSize'] = $rangeSize;
-        $redeemPointsResult[0]['range'] = $range;
-        $redeemPointsResult[0]['startPage'] = $startPage;
-        $redeemPointsResult[0]['endPage'] = $endPage > $pageCnt ? $pageCnt : $endPage;
-        $redeemPointsResult[0]['prev'] = $prev;
-        $redeemPointsResult[0]['next'] = $next;
-        $redeemPointsResult[0]['prevPage'] = $startPage - 1;
-        $redeemPointsResult[0]['nextPage'] = $endPage + 1;
-        return $redeemPointsResult;
+        $pageData[0]['pageCnt'] = $pageCnt;
+        $pageData[0]['listSize'] = $listSize;
+        $pageData[0]['listCnt'] = $listCnt;
+        $pageData[0]['rangeSize'] = $rangeSize;
+        $pageData[0]['range'] = $range;
+        $pageData[0]['startPage'] = $startPage;
+        $pageData[0]['endPage'] = $endPage > $pageCnt ? $pageCnt : $endPage;
+        $pageData[0]['prev'] = $prev;
+        $pageData[0]['next'] = $next;
+        $pageData[0]['prevPage'] = $startPage - 1;
+        $pageData[0]['nextPage'] = $endPage + 1;
+        return $pageData;
     }
 }
