fc5be24e68c6629aa7bf9ab6611a3d256ac09526
soniapark <soniapark@eguanacommerce.com>
2020-12-23 08:25:10 +0900

    remove AllPayLogisticsID param

--- app/code/Eguana/GWLogistics/Model/Request/UnimartCreateReverseShipmentOrder.php
+++ app/code/Eguana/GWLogistics/Model/Request/UnimartCreateReverseShipmentOrder.php
@@ -18,7 +18,8 @@ class UnimartCreateReverseShipmentOrder extends \Eguana\GWLogistics\Model\Reques
     {
         $order = $this->_orderRepository->get($rma->getOrderId());
         $merchantId = $this->_helper->getMerchantId($order->getStoreId());
-        $allPayLogisticsId = $rma->getIncrementId();
+//        $allPayLogisticsId = $rma->getIncrementId();
+        $allPayLogisticsId = '';
         $serverReplyURL = $this->_helper->getReverseLogisticsOrderReplyUrl($order->getStoreId());
 
         $goodsName = $this->_helper->getGoodsNamePrefix($order->getStoreId()) . ' Item X ' . (string)$this->getItemCount($order);
