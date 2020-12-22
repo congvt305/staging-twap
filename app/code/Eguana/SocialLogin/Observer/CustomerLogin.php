e4a3b4da3a113d7a4fc41c089bc747891d4c02c0
Muhammad Umer Farooq <umerfarooq@eguanacommerce.com>
2020-12-22 18:17:58 +0500

    fixed line id not getting saved in admin

--- app/code/Eguana/SocialLogin/Observer/CustomerLogin.php
+++ app/code/Eguana/SocialLogin/Observer/CustomerLogin.php
@@ -134,7 +134,7 @@ class CustomerLogin implements ObserverInterface
                 $this->customerRepository->save($customer);
             } catch (\Exception $e) {
                 $this->messageManager->addErrorMessage(
-                    __('We can not save LINE Information. ') . $e->getMessage()
+                    __('We can not save LINE Information.') . $e->getMessage()
                 );
             }
             $users = $this->socialLoginRepository->getAlreadyLinkedCustomer($customerId, $socialMediaType, $socialId);
