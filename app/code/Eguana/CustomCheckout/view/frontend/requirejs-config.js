701ba1574a6b8449f13bcfe38e207e64abb68da2
Shaukat <raheelshoukat@eguanacommerce.com>
2021-05-21 21:50:35 +0500

    Fixed amount on whole cart rule issue with bundle product fixed

---
+++ app/code/Eguana/CustomCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,16 @@
+/**
+ * @author Eguana Team
+ * @copyriht Copyright (c) 2020 Eguana {http://eguanacommerce.com}
+ * Created by PhpStorm
+ * User: raheel
+ * Date: 20/5/21
+ * Time: 6:00 PM
+ */
+
+var config = {
+    map: {
+        '*': {
+            updateCartTotals: 'Eguana_CustomCheckout/js/cart/updateCartTotals'
+        }
+    }
+};
