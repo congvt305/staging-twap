30517848835e76fbecc063a1832623ceccea7c0c
Phat Pham <phamanhphat@amorepacific.com>
2021-06-23 21:53:15 +0700

    VNMGDC-14 remove check referer, add session for check register success

--- app/code/Amore/CustomerRegistration/Block/Datalayer.php
+++ app/code/Amore/CustomerRegistration/Block/Datalayer.php
@@ -9,6 +9,7 @@
 namespace Amore\CustomerRegistration\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Customer\Model\Session;
 
 /**
  * Class Datalayer
@@ -20,24 +21,49 @@ class Datalayer extends Template
      * @var \Magento\Framework\App\Response\RedirectInterface
      */
     protected $redirect;
+    /**
+     * @var Session
+     */
+    protected $session;
 
     /**
      * Datalayer constructor.
      * @param \Magento\Framework\App\Response\RedirectInterface $redirect
+     * @param Session $customerSession
      * @param Template\Context $context
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\App\Response\RedirectInterface $redirect,
+        Session $customerSession,
         Template\Context $context,
         array $data = [])
     {
         $this->redirect = $redirect;
+        $this->session = $customerSession;
         parent::__construct($context, $data);
     }
 
+    /**
+     * @return string
+     */
     public function getRefererUrl() {
         return $this->redirect->getRefererUrl();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEventRegisterSuccess() {
+        return $this->session->getEventRegisterSuccess();
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function setEventRegisterSuccess($value) {
+        return $this->session->setEventRegisterSuccess($value);
     }
 
 }
