761df2bc34b58a864b684f054c19c258948fb82a
arslan <arslaniqbal@eguanacommerce.com>
2021-07-06 23:37:38 +0500

    fix change email password and phone no. error at VN site

--- app/code/Amore/CustomerRegistration/ViewModel/Edit.php
+++ app/code/Amore/CustomerRegistration/ViewModel/Edit.php
@@ -10,8 +10,10 @@
 
 namespace Amore\CustomerRegistration\ViewModel;
 
-use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Amore\CustomerRegistration\Helper\Data;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * To provide the differnet functions during account update
@@ -21,9 +23,24 @@ use Amore\CustomerRegistration\Helper\Data;
 class Edit extends \Magento\Directory\Block\Data implements ArgumentInterface
 {
     /**
+     * Constant
+     */
+    const DATE_FORMAT = 'd.m.Y';
+
+    /**
      * @var Data
      */
     private $configHelper;
+
+    /**
+     * @var TimezoneInterface
+     */
+    private $timezone;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
 
     public function __construct(
         Data $configHelper,
@@ -33,6 +50,8 @@ class Edit extends \Magento\Directory\Block\Data implements ArgumentInterface
         \Magento\Framework\App\Cache\Type\Config $configCacheType,
         \Magento\Directory\Model\ResourceModel\Region\CollectionFactory $regionCollectionFactory,
         \Magento\Directory\Model\ResourceModel\Country\CollectionFactory $countryCollectionFactory,
+        TimezoneInterface $timezone,
+        StoreManagerInterface $storeManager,
         array $data = []
     ) {
         parent::__construct(
@@ -45,6 +64,8 @@ class Edit extends \Magento\Directory\Block\Data implements ArgumentInterface
             $data
         );
         $this->configHelper = $configHelper;
+        $this->timezone = $timezone;
+        $this->storeManager = $storeManager;
     }
 
     public function getCustomAttributeValue($customer, $attributeCode)
@@ -152,5 +173,19 @@ class Edit extends \Magento\Directory\Block\Data implements ArgumentInterface
     public function getNotAllowedCharList()
     {
         return $this->configHelper->getNotAllowedCharList();
+    }
+
+    /**
+     * Change the date format for before save if the website is Veitnam
+     * @param $date
+     */
+    public function changeDateFormat($date)
+    {
+        $websiteId = (int)$this->storeManager->getStore()->getId();
+        $websiteCode = $this->storeManager->getWebsite($websiteId)->getCode();
+        if ($websiteCode == 'vn_laneige_website') {
+            return $this->timezone->date(new \DateTime($date))->format(self::DATE_FORMAT);
+        }
+        return $date;
     }
 }
