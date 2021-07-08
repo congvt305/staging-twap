dbeffaa777da92cface3a25b579a73faf77e0986
soniapark <soniapark@eguanacommerce.com>
2021-07-08 12:10:06 +0900

    fix website scope issue

--- app/code/Eguana/GWLogistics/Block/Adminhtml/Rma/Edit/Tab/General/ShippingMethod.php
+++ app/code/Eguana/GWLogistics/Block/Adminhtml/Rma/Edit/Tab/General/ShippingMethod.php
@@ -8,9 +8,57 @@
 
 namespace Eguana\GWLogistics\Block\Adminhtml\Rma\Edit\Tab\General;
 
+use Eguana\GWLogistics\Helper\Data;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+use Magento\Rma\Helper\Data as DataAlias1;
+use Magento\Tax\Helper\Data as DataAlias;
+use Magento\Rma\Model\ShippingFactory;
 
 class ShippingMethod extends \Magento\Rma\Block\Adminhtml\Rma\Edit\Tab\General\Shippingmethod
 {
+    /**
+     * @var Data
+     */
+    private $helper;
+
+    /**
+     * ShippingMethod constructor.
+     * @param Context $context
+     * @param Registry $registry
+     * @param DataAlias $taxData
+     * @param DataAlias1 $rmaData
+     * @param ShippingFactory $shippingFactory
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param array $data
+     * @param Json|null $json
+     * @param Data $helper
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        DataAlias $taxData,
+        DataAlias1 $rmaData,
+        ShippingFactory $shippingFactory,
+        PriceCurrencyInterface $priceCurrency,
+        array $data = [],
+        Json $json = null,
+        Data $helper
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $taxData,
+            $rmaData,
+            $shippingFactory,
+            $priceCurrency,
+            $data,
+            $json
+        );
+        $this->helper = $helper;
+    }
 
     /**
      * @param \Magento\Rma\Api\Data\RmaInterface $rma
@@ -31,4 +79,13 @@ class ShippingMethod extends \Magento\Rma\Block\Adminhtml\Rma\Edit\Tab\General\S
         return $this->getUrl('eguana_gwlogistics/reverseorder/create', ['rma_id' => $rma->getEntityId()]);
     }
 
+    /**
+     * This method is used to get the module active status
+     * @param $rma
+     * @return mixed
+     */
+    public function getIsEnabled($rma)
+    {
+        return $this->helper->isActive($rma->getStoreId());
+    }
 }
