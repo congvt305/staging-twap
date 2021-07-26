cd7675967f181c9cfc0d4cff4ed302f1d8985a7a
a4deel <adeelahmad@eguanacommerce.com>
2021-07-26 05:10:56 -0700

    Module Placed In Amore

--- app/etc/config.php
+++ app/etc/config.php
@@ -531,6 +531,7 @@ return [
         'Amore_Base' => 1,
         'Eguana_Base' => 1,
         'Amore_GaTagging' => 1,
+        'Amore_GcrmDataExport' => 1,
         'Amore_PointsIntegration' => 1,
         'Ecpay_Ecpaypayment' => 1,
         'Dotdigitalgroup_Email' => 1,
