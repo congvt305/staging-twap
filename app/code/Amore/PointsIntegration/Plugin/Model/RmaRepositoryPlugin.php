f9a8a856d71ad94e9b764d25f48d318ce76f387c
long <longht2010@amorepacific.com>
2021-08-10 09:30:29 +0700

    VNMGDC-15 sycn paid, cancelled and return orders with POS

--- app/code/Amore/PointsIntegration/Plugin/Model/RmaRepositoryPlugin.php
+++ app/code/Amore/PointsIntegration/Plugin/Model/RmaRepositoryPlugin.php
@@ -9,80 +9,58 @@
 namespace Amore\PointsIntegration\Plugin\Model;
 
 use Amore\PointsIntegration\Exception\PosPointsException;
+use Amore\PointsIntegration\Logger\Logger;
+use Amore\PointsIntegration\Model\Source\Config;
 use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Rma\Model\RmaRepository;
 use Magento\Store\Model\StoreManagerInterface;
 
 class RmaRepositoryPlugin
 {
     /**
-     * @var \Amore\PointsIntegration\Model\Source\Config
+     * @var Config
      */
     private $config;
     /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-    /**
-     * @var \Amore\PointsIntegration\Logger\Logger
+     * @var Logger
      */
     private $logger;
-    /**
-     * @var \Magento\Framework\Event\ManagerInterface
-     */
-    private $eventManager;
-    /**
-     * @var Json
-     */
-    private $json;
-    /**
-     * @var \Amore\PointsIntegration\Model\PosReturnData
-     */
-    private $posReturnData;
-    /**
-     * @var \Amore\PointsIntegration\Model\Connection\Request
-     */
-    private $request;
 
     /**
-     * RmaPlugin constructor.
-     * @param \Amore\PointsIntegration\Model\Source\Config $config
-     * @param StoreManagerInterface $storeManager
-     * @param \Amore\PointsIntegration\Logger\Logger $logger
-     * @param \Magento\Framework\Event\ManagerInterface $eventManager
-     * @param Json $json
-     * @param \Amore\PointsIntegration\Model\PosReturnData $posReturnData
-     * @param \Amore\PointsIntegration\Model\Connection\Request $request
+     * @param Config $config
+     * @param Logger $logger
      */
     public function __construct(
-        \Amore\PointsIntegration\Model\Source\Config $config,
-        StoreManagerInterface $storeManager,
-        \Amore\PointsIntegration\Logger\Logger $logger,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        Json $json,
-        \Amore\PointsIntegration\Model\PosReturnData $posReturnData,
-        \Amore\PointsIntegration\Model\Connection\Request $request
+        Config $config,
+        Logger $logger
     )
     {
         $this->config = $config;
-        $this->storeManager = $storeManager;
         $this->logger = $logger;
-        $this->eventManager = $eventManager;
-        $this->json = $json;
-        $this->posReturnData = $posReturnData;
-        $this->request = $request;
     }
 
-    public function afterSave(\Magento\Rma\Model\RmaRepository $subject, $rma)
+    /**
+     * @param RmaRepository $subject
+     * @param $rma
+     * @return mixed
+     */
+    public function afterSave(RmaRepository $subject, $rma)
     {
         $websiteId = $rma->getOrder()->getStore()->getWebsiteId();
         $moduleEnableCheck = $this->config->getActive($websiteId);
         $rmaSendingEnableCheck = $this->config->getPosRmaActive($websiteId);
         $completedStatus = 'processed_closed';
         $posRmaCompletedSent = $rma->getData('pos_rma_completed_sent');
+        $posRmaCompletedSend = $rma->getData('pos_rma_completed_send');
 
         if ($moduleEnableCheck && $rmaSendingEnableCheck) {
-            if (!$posRmaCompletedSent && $rma->getStatus() == $completedStatus) {
-                $rma->setData('pos_rma_completed_send', true);
+            if (!$posRmaCompletedSent && !$posRmaCompletedSend && $rma->getStatus() == $completedStatus) {
+                try {
+                    $rma->setData('pos_rma_completed_send', true);
+                    $subject->save($rma);
+                } catch (\Exception $exception) {
+                    $this->logger->error('POS ERROR: ' . $exception->getMessage());
+                }
             }
         }
 
