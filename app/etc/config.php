a078908bf9a0525003a174f0936a8d33cf3a9fb3
Phat Pham <phamanhphat@amorepacific.com>
2021-08-26 18:23:23 +0700

    VNMGDC-109 remove JS to Minification because it has error

--- app/etc/config.php
+++ app/etc/config.php
@@ -127,7 +127,7 @@ return [
                 ],
                 'js' => [
                     'merge_files' => '0',
-                    'minify_files' => '1',
+                    'minify_files' => '0',
                     'minify_exclude' => [
                         'tiny_mce' => '/tiny_mce/',
                         'cardinal_commerce' => '/v1/songbird',
