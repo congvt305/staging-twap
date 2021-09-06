e59865a363592ad95e9bd8746a0e799c30d7b927
Phat Pham <phamanhphat@amorepacific.com>
2021-09-06 17:18:17 +0700

    VNMGDC-79 add try catch for redemption not found: refactor

--- app/code/Eguana/Redemption/Controller/Details/Index.php
+++ app/code/Eguana/Redemption/Controller/Details/Index.php
@@ -198,15 +198,14 @@ class Index extends Action
     public function execute()
     {
         $defaultStoreId = $this->storeManager->getStore()->getId();
+        $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
         if ($this->redemptionConfig->getEnableValue('enabled', $defaultStoreId) == 0) {
             $this->messageManager->addSuccessMessage(__('The redemption module is disable by admin.'));
-            $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
             return $resultRedirect->setUrl('/');
         }
         $date = $this->date->gmtDate();
         if ($this->getRequest()->getParam('confirm') == 1) {
             $counter = $this->counter->load($this->getRequest()->getParam('counter_id'));
-            $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
             if ($counter && $counter->getRedemptionId()) {
                 $counter->setStatus(Counter::STATUS_REDEMPTION);
                 $counter->setRedeemDate($date);
@@ -230,32 +229,22 @@ class Index extends Action
                 $this->messageManager->addErrorMessage(
                     __('This redemption has been expired!')
                 );
-                $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
                 return $resultRedirect->setUrl('/');
             } elseif ($redemptionStartDate > $currentDate && $redemptionEndDate > $currentDate) {
                 $this->messageManager->addErrorMessage(
                     __('This redemption will be available at %1', $redemptionStartDate)
                 );
-                $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
                 return $resultRedirect->setUrl('/');
             }
-            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-            if (isset($redemptionId)) {
-                $redemption = $this->redemptionRepository->getById($redemptionId);
-                if (empty($redemption->getData()) || $redemption->isActive() == 0) {
+            if ($redemptionId) {
+                if (empty($redemptionDetail->getData()) || $redemptionDetail->isActive() == 0) {
                     $this->managerInterface->addErrorMessage(__('This redemption is not available.'));
-                    $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
                     $resultRedirect->setUrl('/');
                     return $resultRedirect;
                 }
-            } elseif (!isset($redemptionId)) {
-                $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
-                $resultRedirect->setUrl('/');
-                return $resultRedirect;
             }
         } catch (\Exception $exception) {
             $this->managerInterface->addErrorMessage(__('This redemption is not available.'));
-            $resultRedirect = $this->result->create(ResultFactory::TYPE_REDIRECT);
             $resultRedirect->setUrl('/');
             return $resultRedirect;
         }
