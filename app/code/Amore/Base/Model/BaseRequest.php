b06ba6e33aba9ad35074fe389283f37a1e9a31f7
nghiepthan <nghiepthan@amorepacific.com>
2021-11-30 11:50:16 +0700

    [VNMGDC-137]_update abstract request and sap interfaces

---
+++ app/code/Amore/Base/Model/BaseRequest.php
@@ -0,0 +1,111 @@
+<?php
+namespace Amore\Base\Model;
+
+use CJ\Middleware\Helper\Data as MiddlewareHelper;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\HTTP\Client\Curl;
+use Magento\Framework\Serialize\Serializer\Json;
+
+abstract class BaseRequest
+{
+    /**
+     * @var Curl
+     */
+    protected $curl;
+    /**
+     * @var Json
+     */
+    protected $json;
+
+    /**
+     * @var MiddlewareHelper
+     */
+    protected $middlewareHelper;
+
+    /**
+     * Request constructor.
+     * @param Curl $curl
+     * @param Json $json
+     * @param MiddlewareHelper $middlewareHelper
+     */
+    public function __construct(
+        Curl $curl,
+        Json $json,
+        MiddlewareHelper $middlewareHelper
+    ) {
+        $this->curl = $curl;
+        $this->json = $json;
+        $this->middlewareHelper = $middlewareHelper;
+    }
+
+    /**
+     * Send request via middleware
+     * @param $url
+     * @param $requestData
+     * @param string $scope
+     * @param $websiteId
+     * @param string $type
+     * @return string
+     */
+    public function send($url, $requestData, $scope = 'store', $websiteId, $type = 'confirm')
+    {
+        $isNewMiddlewareEnable = $this->middlewareHelper->isNewMiddlewareEnabled('website', $websiteId);
+        if ($isNewMiddlewareEnable) {
+            if (!is_array($requestData)) {
+                $requestData = $this->json->unserialize($requestData);
+            }
+            $url = $this->middlewareHelper->getNewMiddlewareURL($scope, $websiteId);
+            $requestData['APP_ID'] = $this->getInterfaceID($scope, $websiteId, $type);
+            $requestData['API_USER_ID'] = $this->middlewareHelper->getMiddlewareUsername($scope, $websiteId);
+            $requestData['AUTH_KEY'] = $this->middlewareHelper->getMiddlewareAuthKey($scope, $websiteId);
+        }
+        $this->curl->post($url, $this->json->serialize($requestData));
+
+        return $this->curl->getBody();
+    }
+
+    /**
+     * Get Interface Id for replacing path corresponding
+     * @param $scope
+     * @param $websiteId
+     * @param $type
+     * @return mixed
+     */
+    public function getInterfaceID($scope, $websiteId, $type)
+    {
+        $path = '';
+        switch ($type) {
+            case 'memberSearch':
+                $path = $this->middlewareHelper->getMemberSearchInterfaceId($scope, $websiteId);
+                break;
+            case 'redeemSearch':
+                $path = $this->middlewareHelper->getRedeemSearchInterfaceId($scope, $websiteId);
+                break;
+            case 'pointSearch':
+                $path = $this->middlewareHelper->getPointSearchInterfaceId($scope, $websiteId);
+                break;
+            case 'customerOrder':
+                $path = $this->middlewareHelper->getCustomerSearchInterfaceId($scope, $websiteId);
+                break;
+            case 'confirm':
+                $path = $this->middlewareHelper->getOrderConfirmInterfaceId($scope, $websiteId);
+                break;
+            case 'cancel':
+                $path = $this->middlewareHelper->getOrderCancelInterfaceId($scope, $websiteId);
+                break;
+            case 'memberInfo':
+                $path = $this->middlewareHelper->getMemberInfoInterfaceId($scope, $websiteId);
+                break;
+            case 'memberJoin':
+                $path = $this->middlewareHelper->getMemberJoinInterfaceId($scope, $websiteId);
+                break;
+            case 'baInfo':
+                $path = $this->middlewareHelper->getBacodeInfoInterfaceId($scope, $websiteId);
+                break;
+            default:
+                $path = $this->middlewareHelper->getOrderConfirmInterfaceId($scope, $websiteId);
+        }
+        return $path;
+    }
+
+}
--- app/code/Amore/Sap/Model/Connection/Request.php
+++ app/code/Amore/Sap/Model/Connection/Request.php
@@ -8,14 +8,16 @@
 
 namespace Amore\Sap\Model\Connection;
 
+use Braintree\Base;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\HTTP\Client\Curl;
 use Magento\Framework\Serialize\Serializer\Json;
 use Amore\Sap\Model\Source\Config;
 use Amore\Sap\Logger\Logger;
 use CJ\Middleware\Helper\Data as MiddlewareHelper;
+use Amore\Base\Model\BaseRequest;
 
-class Request
+class Request extends BaseRequest
 {
     const URL_REQUEST = 'sap/general/url';
 
@@ -24,14 +26,6 @@ class Request
     const ORDER_CANCEL_PATH = 'sap/url_path/order_cancel_path';
 
     /**
-     * @var Curl
-     */
-    private $curl;
-    /**
-     * @var Json
-     */
-    private $json;
-    /**
      * @var Config
      */
     private $config;
@@ -39,11 +33,6 @@ class Request
      * @var Logger
      */
     private $logger;
-
-    /**
-     * @var MiddlewareHelper
-     */
-    protected $middlewareHelper;
 
     /**
      * @param Curl $curl
@@ -59,11 +48,9 @@ class Request
         Logger $logger,
         MiddlewareHelper $middlewareHelper
     ) {
-        $this->curl = $curl;
-        $this->json = $json;
+        parent::__construct($curl, $json, $middlewareHelper);
         $this->config = $config;
         $this->logger = $logger;
-        $this->middlewareHelper = $middlewareHelper;
     }
 
     public function postRequest($requestData, $storeId, $type = 'confirm')
@@ -71,15 +58,6 @@ class Request
         $url = $this->getUrl($storeId);
         $path = $this->getPath($storeId, $type);
         $fullUrl = $url . $path;
-        $isNewMiddlewareEnable = $this->middlewareHelper->isNewMiddlewareEnabled('store', $storeId);
-        if ($isNewMiddlewareEnable) {
-            $fullUrl = $this->middlewareHelper->getNewMiddlewareURL('store', $storeId);
-            $requestData = $this->json->unserialize($requestData);
-            $requestData['APP_ID'] = $this->getInterfaceID($storeId, $type);
-            $requestData['API_USER_ID'] = $this->middlewareHelper->getMiddlewareUsername('store', $storeId);
-            $requestData['AUTH_KEY'] = $this->middlewareHelper->getMiddlewareAuthKey('store', $storeId);
-            $requestData = $this->json->serialize($requestData);
-        }
         if ($this->config->getLoggingCheck()) {
             $this->logger->info('LIVE MODE REQUEST');
             $this->logger->info($requestData);
@@ -101,9 +79,7 @@ class Request
                     $this->curl->setOption(CURLOPT_SSL_VERIFYPEER, false);
                 }
 
-                $this->curl->post($fullUrl, $requestData);
-
-                $response = $this->curl->getBody();
+                $response = $this->send($fullUrl, $requestData, 'store', $storeId, $type);
 
                 if ($this->config->getLoggingCheck()) {
                     $this->logger->info('LIVE RESPONSE');
@@ -143,21 +119,6 @@ class Request
                 break;
             default:
                 $path = $this->config->getValue(self::ORDER_CONFIRM_PATH, 'store', $storeId);
-        }
-        return $path;
-    }
-
-    public function getInterfaceID($storeId, $type)
-    {
-        switch ($type) {
-            case 'confirm':
-                $path = $this->middlewareHelper->getOrderConfirmInterfaceId('store', $storeId);
-                break;
-            case 'cancel':
-                $path = $this->middlewareHelper->getOrderCancelInterfaceId('store', $storeId);
-                break;
-            default:
-                $path = $this->middlewareHelper->getOrderConfirmInterfaceId('store', $storeId);
         }
         return $path;
     }
