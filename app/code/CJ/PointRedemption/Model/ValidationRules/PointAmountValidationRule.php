bd084038c57f6b53755a4558215d320f161f6945
long <longht2010@amorepacific.com>
2021-12-07 12:03:06 +0700

    VNMGDC-83 factory point validation

--- app/code/CJ/PointRedemption/Model/ValidationRules/PointAmountValidationRule.php
+++ app/code/CJ/PointRedemption/Model/ValidationRules/PointAmountValidationRule.php
@@ -8,8 +8,7 @@ declare(strict_types=1);
 
 namespace CJ\PointRedemption\Model\ValidationRules;
 
-use Amore\PointsIntegration\Model\CustomerPointsSearch;
-use CJ\PointRedemption\Setup\Patch\Data\AddRedemptionAttributes;
+use CJ\PointRedemption\Helper\Data;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Validation\ValidationResult;
 use Magento\Framework\Validation\ValidationResultFactory;
@@ -32,9 +31,9 @@ class PointAmountValidationRule implements QuoteValidationRuleInterface
     private $validationResultFactory;
 
     /**
-     * @var CustomerPointsSearch
+     * @var Data
      */
-    protected CustomerPointsSearch $customerPointsSearch;
+    private Data $pointRedemptionHelper;
 
     /**
      * @param ValidationResultFactory $validationResultFactory
@@ -42,12 +41,12 @@ class PointAmountValidationRule implements QuoteValidationRuleInterface
      */
     public function __construct(
         ValidationResultFactory $validationResultFactory,
-        CustomerPointsSearch $customerPointsSearch,
+        Data $pointRedemptionHelper,
         string $generalMessage = ''
     ) {
         $this->validationResultFactory = $validationResultFactory;
+        $this->pointRedemptionHelper = $pointRedemptionHelper;
         $this->generalMessage = $generalMessage;
-        $this->customerPointsSearch = $customerPointsSearch;
     }
 
     /**
@@ -58,54 +57,10 @@ class PointAmountValidationRule implements QuoteValidationRuleInterface
     public function validate(Quote $quote): array
     {
         $validationErrors = [];
-        $validationResult = true;
-        if ($this->getQuoteUsedPointAmount($quote) > 0) {
-            $validationResult = $this->validatePointBalance($quote);
-        }
-        if (!$validationResult) {
-            $validationErrors = [__($this->generalMessage)];
+        if ($this->pointRedemptionHelper->getQuoteUsedPointAmount($quote) > 0) {
+            $this->pointRedemptionHelper->validatePointBalance(0, $quote);
         }
 
         return [$this->validationResultFactory->create(['errors' => $validationErrors])];
-    }
-
-    /**
-     * @return bool
-     * @throws LocalizedException
-     */
-    private function validatePointBalance(Quote $quote)
-    {
-        $customerId = $quote->getCustomerId();
-        $websiteId = $quote->getStore()->getWebsiteId();
-        $memberPointInfo = $this->customerPointsSearch->getMemberSearchResult($customerId, $websiteId);
-        if (!isset($memberPointInfo['data']['availablePoint'])) {
-            throw new LocalizedException(
-                __(
-                    "Point service is not available now, please try later. Sorry for the inconvenient"
-                )
-            );
-        }
-        $balanceAmount = (int)$memberPointInfo['data']['availablePoint'];
-        $usedPointAmount = $this->getQuoteUsedPointAmount($quote);
-
-        return $balanceAmount >= $usedPointAmount;
-    }
-
-    /**
-     * @param $quote
-     * @return float|int|mixed
-     */
-    private function getQuoteUsedPointAmount(Quote $quote)
-    {
-        $usedAmount = 0;
-        foreach ($quote->getAllVisibleItems() as $item) {
-            $pointAmount = $item->getData(AddRedemptionAttributes::POINT_REDEMPTION_AMOUNT_ATTRIBUTE_CODE);
-            $isRedeemableProduct = $item->getData(AddRedemptionAttributes::IS_POINT_REDEEMABLE_ATTRIBUTE_CODE);
-            if ($isRedeemableProduct && $pointAmount) {
-                $usedAmount = $usedAmount + ($pointAmount * $item->getQty());
-            }
-        }
-
-        return $usedAmount;
     }
 }
