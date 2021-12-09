a4cf3e3cef9107075a15471385c973535e36a04b
nghiepthan <nghiepthan@amorepacific.com>
2021-12-09 08:45:57 +0700

    [VNMGDC-137]_remove curl options makes the request takes much time

--- app/code/Amore/CustomerRegistration/Model/POSSystem.php
+++ app/code/Amore/CustomerRegistration/Model/POSSystem.php
@@ -173,9 +173,6 @@ class POSSystem extends BaseRequest
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "POST",
                 CURLOPT_HTTPHEADER => [
                     'Content-type: application/json'
@@ -388,9 +385,6 @@ class POSSystem extends BaseRequest
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "POST",
                 CURLOPT_HTTPHEADER => [
                     'Content-type: application/json'
@@ -538,9 +532,6 @@ class POSSystem extends BaseRequest
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "POST",
                 CURLOPT_HTTPHEADER => [
                     'Content-type: application/json'
