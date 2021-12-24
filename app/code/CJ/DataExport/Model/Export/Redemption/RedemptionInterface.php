0a7ccc57c9423cfab9ae1d6ddcef49b02c483be0
cjolivenetworks.황판부 <hoangphan@amorepacific.com>
2021-12-24 17:17:52 +0900

    Vnmgdc 269 data collect query request staging

---
+++ app/code/CJ/DataExport/Model/Export/Redemption/RedemptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CJ\DataExport\Model\Export\Redemption;
+
+/**
+ * Interface RedemptionInterface
+ */
+interface RedemptionInterface
+{
+    const REDEEM_ID = 'redeem_id';
+    const REDEMPTION_CAMPAIGN_ID = 'redemption_campaign_id';
+    const REDEMPTION_CAMPAIGN_NAME = 'redemption_campaign_name';
+    const REDEEM_DATE = 'redeem_date';
+    const NAME = 'name';
+    const TELEPHONE = 'telephone';
+    const STATUS = 'status';
+    const STORE_COUNTER = 'store_counter';
+}
---
+++ app/code/CJ/DataExport/Model/Export/Rma/RmaInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CJ\DataExport\Model\Export\Rma;
+
+/**
+ * Interface RmaInterface
+ */
+interface RmaInterface
+{
+    const RMA_ID = 'increment_id';
+    const REQUESTED_DATE = 'requested_date';
+    const ORDER_ID = 'order_id';
+    const SHIPPING_METHOD_NUMBER = 'shipping_method_number';
+    const PAYMENT_METHOD = 'payment_method';
+}
