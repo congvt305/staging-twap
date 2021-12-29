4e20c1c07eec330a24d3c2250414b98688567c51
long <longht2010@amorepacific.com>
2021-12-29 16:09:50 +0700

    VNMGDC-83 add member gift text in checkout page

--- app/code/CJ/PointRedemption/Plugin/ProductUrl.php
+++ app/code/CJ/PointRedemption/Plugin/ProductUrl.php
@@ -3,9 +3,20 @@
 namespace CJ\PointRedemption\Plugin;
 
 use CJ\PointRedemption\Setup\Patch\Data\AddIsMembershipCategoryAttribute as MembershipCategory;
+use Magento\Framework\App\RequestInterface;
 
 class ProductUrl
 {
+    /**
+     * @var RequestInterface
+     */
+    protected $request;
+
+    public function __construct(RequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
     /**
      * Add query ?point=1 to product url in membership category
      * @param $subject
@@ -15,11 +26,9 @@ class ProductUrl
      */
     public function beforeGetUrl($subject, $product, $params = [])
     {
-        $currentCategory = $product->getCategory();
-        $isMembershipCategory = $currentCategory
-            ? $currentCategory->getData(MembershipCategory::CATEGORY_ATTRIBUTE_CODE_IS_MEMBERSHIP)
-            : null;
-        if ($isMembershipCategory) {
+        $isMembershipCategory = $this->isMembershipCategory($product);
+        $isPointRedemptionRequest = $this->isPointRedemptionRequest();
+        if ($isMembershipCategory || $isPointRedemptionRequest) {
             if (!isset($params['_query'])) {
                 $params['_query'] = [];
             }
@@ -27,5 +36,26 @@ class ProductUrl
         }
 
         return [$product, $params];
+    }
+
+    /**
+     * @param $product
+     * @return false|mixed
+     */
+    private function isMembershipCategory($product)
+    {
+        $currentCategory = $product->getCategory();
+        return $currentCategory
+            ? $currentCategory->getData(MembershipCategory::CATEGORY_ATTRIBUTE_CODE_IS_MEMBERSHIP)
+            : false;
+    }
+
+    /**
+     * @return false|mixed
+     */
+    private function isPointRedemptionRequest()
+    {
+        $postData = $this->request->getPostValue();
+        return $postData['is_point_redemption'] ?? false;
     }
 }
