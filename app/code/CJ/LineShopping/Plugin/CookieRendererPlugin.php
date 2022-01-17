6cf8df0bfb196332c6ee25d81c144303352341c0
jexchan <jexchan@gmail.com>
2022-01-17 16:14:01 +0530

    VNMGDC-318 fix bug set cookie

--- app/code/CJ/LineShopping/Plugin/CookieRendererPlugin.php
+++ app/code/CJ/LineShopping/Plugin/CookieRendererPlugin.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace CJ\LineShopping\Block;
+namespace CJ\LineShopping\Plugin;
 
 use CJ\LineShopping\Helper\CookieLineInformation;
 use CJ\LineShopping\Helper\Config;
 use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Framework\View\Element\Template;
 use Exception;
 use CJ\LineShopping\Logger\Logger;
+use Magento\Framework\App\FrontControllerInterface;
+use Magento\Framework\App\RequestInterface;
 
-class CookieRenderer extends Template
+class CookieRendererPlugin
 {
     /**
      * @var Json
@@ -32,36 +33,48 @@ class CookieRenderer extends Template
     protected Logger $logger;
 
     /**
+     * @var RequestInterface
+     */
+    protected RequestInterface $request;
+
+    /**
+     * @param RequestInterface $request
      * @param Logger $logger
      * @param CookieLineInformation $cookieLineInformation
      * @param Config $config
      * @param Json $json
-     * @param Template\Context $context
-     * @param array $data
      */
     public function __construct(
+        RequestInterface $request,
         Logger $logger,
         CookieLineInformation $cookieLineInformation,
         Config $config,
-        Json $json,
-        Template\Context $context,
-        array $data = []
+        Json $json
     ) {
+        $this->request = $request;
         $this->logger = $logger;
         $this->cookieLineInformation = $cookieLineInformation;
         $this->config = $config;
         $this->json = $json;
-        parent::__construct($context, $data);
-        //$this->setLineCookie();
     }
 
     /**
+     * @param FrontControllerInterface $subject
+     * @param $result
      * @return void
      */
-    public function setLineCookie()
+    public function afterDispatch(FrontControllerInterface $subject, $result)
+    {
+        $this->setLineCookie($subject);
+        return $result;
+    }
+    /**
+     * @return void
+     */
+    public function setLineCookie($subject)
     {
         try {
-            $params = $this->getRequest()->getParams();
+            $params = $this->request->getParams();
             if ($this->config->isEnable() && isset($params['ecid'])) {
                 $duration = $this->config->getCookieLifeTime();
                 $this->cookieLineInformation->setCookie(CookieLineInformation::LINE_SHOPPING_ECID_COOKIE_NAME, $params['ecid'] , $duration);
