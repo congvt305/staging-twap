05af0fb0662b2bf34ab1a5586ba31c8d4d49483a
CJ Dat Nguyen <datnguyen@amorepacific.com>
2022-01-24 15:39:33 +0530

    VNMGDC-331 Laneige Mall Setup Hover Image change base on comment

--- app/code/CJ/HoverImage/Block/HoverImage.php
+++ app/code/CJ/HoverImage/Block/HoverImage.php
@@ -6,6 +6,7 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Catalog\Block\Product\ListProduct;
+use Magento\Framework\App\Request\Http;
 
 class HoverImage extends Template
 {
@@ -14,17 +15,22 @@ class HoverImage extends Template
      */
     protected $listProduct;
     /**
-     * @var
-     */
-    protected $product;
-    /**
      * @var ScopeConfigInterface
      */
     protected $scopeConfig;
     /**
-     *
+     * @var Http
      */
-    const XML_CONFIG_PATH = 'hover_image/general/active';
+    protected $request;
+    /**
+     * xml config path hover image
+     */
+    const XML_CONFIG_PATH_HOVER_IMAGE_ACTIVE = 'hover_image/general/active';
+
+    /**
+     * default xml layout for hover image
+     */
+    const XML_DEFAULT_LAYOUT_HOVER_IMAGE = ['catalog_category_view', 'catalogsearch_result_index'];
 
 
     /**
@@ -33,11 +39,13 @@ class HoverImage extends Template
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
-        ListProduct $listProduct,
+        ListProduct          $listProduct,
         Template\Context     $context,
+        Http                 $request,
         array                $data = []
     ){
         $this->setTemplate('CJ_HoverImage::product/hover_image.phtml');
+        $this->request = $request;
         $this->listProduct = $listProduct;
         $this->scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
@@ -47,9 +55,9 @@ class HoverImage extends Template
      * get data config
      * @return mixed
      */
-    public function isEnable()
+    public function isHoverImageEnabled()
     {
-        $configValue = $this->scopeConfig->getValue(self::XML_CONFIG_PATH,ScopeInterface::SCOPE_WEBSITE);
+        $configValue = $this->scopeConfig->getValue(self::XML_CONFIG_PATH_HOVER_IMAGE_ACTIVE, ScopeInterface::SCOPE_WEBSITE);
         return $configValue;
     }
 
@@ -62,7 +70,24 @@ class HoverImage extends Template
      */
     public function getImage($product, $imageId, $attributes = [])
     {
-        return $this->listProduct->getImage($product,$imageId,$attributes);
+        return $this->listProduct->getImage($product, $imageId, $attributes);
+    }
+
+    /**
+     * @return void
+     */
+    public function getHoverImage($productHoverImage)
+    {
+        $xmlLayout = $this->request->getFullActionName();
+        // check for catalog_category_view.xml and catalogsearch_result_index.xml
+        if((!strpos($productHoverImage->getImageUrl(), 'placeholder') && in_array($xmlLayout,self::XML_DEFAULT_LAYOUT_HOVER_IMAGE))) {
+            return true;
+        }
+        if ((!strpos($productHoverImage->getImageUrl(), 'placeholder') && $this->isHoverImageEnabled())) {
+            return true;
+        }
+        return false;
+
     }
 
 }
