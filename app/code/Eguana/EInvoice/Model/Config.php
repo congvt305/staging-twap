7f68ba4da97a3e5aa564f9fefa2e6dfa024a031e
Hoang Phan <hoangphan@amorepacific.com>
2022-03-29 14:25:56 +0700

    VNMGDC-388【TW SWS】e Invoice issue

---
+++ app/code/Eguana/EInvoice/Model/Config.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Eguana\EInvoice\Model;
+
+/**
+ * Class Config
+ */
+class Config
+{
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    const KEY_MERCHANT_ID = 'MerchantID';
+    const XML_PATH_EINVOICE_HASH_KEY = 'invoice/ecpay_invoice_hash_key';
+    const XML_PATH_EINVOICE_HASH_IV = 'invoice/ecpay_invoice_hash_iv';
+    const XML_PATH_ECPAY_QUERY_TEST_FLAG = 'eguana_einvoice/ecpay_einvoice_issue/ecpay_query_test_flag';
+    const XML_PATH_ECPAY_QUERY_STAGE_URL = 'eguana_einvoice/ecpay_einvoice_issue/ecpay_query_stage_url';
+    const XML_PATH_ECPAY_QUERY_PRODUCTION_URL = 'eguana_einvoice/ecpay_einvoice_issue/ecpay_query_production_url';
+
+    /**
+     * Constructor
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * check whether it is on test mode
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @return bool
+     */
+    protected function getTestFlag(?int $storeId, string $scopeType = 'websites'): bool
+    {
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_ECPAY_QUERY_TEST_FLAG, $scopeType, $storeId);
+    }
+
+    /**
+     * return staging API Endpoint that queries invoice information
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @param string $suffix
+     * @return string
+     */
+    protected function getStageUrl(?int $storeId, string $scopeType = 'default', string $suffix = 'Issue'): string
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_ECPAY_QUERY_STAGE_URL, $scopeType) . $suffix;
+    }
+
+    /**
+     * return production API Endpoint that queries invoice information
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @param string $suffix
+     * @return string
+     */
+    protected function getProdUrl(?int $storeId, string $scopeType = 'default', string $suffix = 'Issue'): string
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_ECPAY_QUERY_PRODUCTION_URL, $scopeType) . $suffix;
+    }
+
+    /**
+     * return config hash key
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @param string $prefix
+     * @return string
+     */
+    public function getHashKey(
+        ?int $storeId,
+        string $scopeType = 'store',
+        string $prefix = "payment/ecpay_ecpaypayment/ecpay_"
+    ): string {
+        return $this->scopeConfig->getValue($prefix . self::XML_PATH_EINVOICE_HASH_KEY, $scopeType, $storeId);
+    }
+
+    /**
+     * return config hash iv
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @param string $prefix
+     * @return string
+     */
+    public function getHashIv(
+        ?int $storeId,
+        string $scopeType = 'store',
+        string $prefix = "payment/ecpay_ecpaypayment/ecpay_"
+    ): string {
+        return $this->scopeConfig->getValue($prefix . self::XML_PATH_EINVOICE_HASH_IV, $scopeType, $storeId);
+    }
+
+    /**
+     * return config merchant id
+     *
+     * @param int|null $storeId
+     * @param string $scopeType
+     * @param string $prefix
+     * @return string
+     */
+    public function getMerchantId(
+        ?int $storeId,
+        string $scopeType = 'store',
+        string $prefix = "payment/ecpay_ecpaypayment/ecpay_"
+    ): string {
+        return $this->scopeConfig->getValue($prefix . self::KEY_MERCHANT_ID, $scopeType, $storeId);
+    }
+
+    /**
+     * @param int|null $storeId
+     * @return string
+     */
+    public function getQueryEInvoiceApiUrl(?int $storeId): string
+    {
+        return $this->getTestFlag($storeId) ? $this->getStageUrl($storeId) : $this->getProdUrl($storeId);
+    }
+}
