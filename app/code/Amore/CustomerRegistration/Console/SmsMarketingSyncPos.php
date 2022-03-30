e61dc03b0e7386ac2e944b87da941ee7d8327a4d
binh nguyen <binhnguyen@amorepacific.com>
2022-03-30 09:26:02 +0530

    VNMGDC-222 change patch to command

--- app/code/Amore/CustomerRegistration/Console/SmsMarketingSyncPos.php
+++ app/code/Amore/CustomerRegistration/Console/SmsMarketingSyncPos.php
@@ -1,28 +1,26 @@
 <?php
 
-namespace CJ\DataPatch\Setup\Patch\Data;
+namespace Amore\CustomerRegistration\Console;
 
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
-use Psr\Log\LoggerInterface;
-use Magento\Store\Model\StoreManagerInterface;
 use Magento\Customer\Model\AddressRegistry;
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
-class MigrationSMSMarketingAgreement implements DataPatchInterface
+class SmsMarketingSyncPos extends \Symfony\Component\Console\Command\Command
 {
+    const NAME = 'pos:smsmarketing:sync';
     const TIME_TO_MIGRATE = '2022-03-24';
     const STORE_CODE_APPLY = ['default', 'tw_laneige'];
+    const QUANTITY = 'qty';
+    const QUANTITY_DEFAULT = 100;
 
     /**
      * @var CustomerCollectionFactory
      */
     protected CustomerCollectionFactory $customerCollectionFactory;
-
-    /**
-     * @var LoggerInterface
-     */
-    protected LoggerInterface $logger;
 
     /**
      * @var StoreManagerInterface
@@ -38,45 +36,68 @@ class MigrationSMSMarketingAgreement implements DataPatchInterface
      * @param CustomerCollectionFactory $customerCollectionFactory
      * @param StoreManagerInterface $storeManager
      * @param AddressRegistry $addressRegistry
-     * @param LoggerInterface $logger
+     * @param string|null $name
      */
     public function __construct(
         CustomerCollectionFactory $customerCollectionFactory,
         StoreManagerInterface $storeManager,
         AddressRegistry $addressRegistry,
-        LoggerInterface $logger
+        string $name = null
     ) {
         $this->customerCollectionFactory = $customerCollectionFactory;
         $this->storeManager = $storeManager;
         $this->addressRegistry = $addressRegistry;
-        $this->logger = $logger;
+        parent::__construct($name);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName(self::NAME)
+            ->setDescription('POS - EC SMS Marketing synchronization');
+        $this->addOption(
+            self::QUANTITY,
+            null,
+            InputOption::VALUE_OPTIONAL, 'Qty to run'
+        );
+        parent::configure();
     }
 
     /**
-     * @return MigrationSMSMarketingAgreement|void
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * {@inheritDoc}
      */
-    public function apply()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $quantity = $input->getOption(self::QUANTITY) ? $input->getOption(self::QUANTITY) : self::QUANTITY_DEFAULT;
+
         $customers = $this->customerCollectionFactory->create()
             ->addAttributeToSelect('*')
             ->addFieldToFilter('created_at', ['lteq' => self::TIME_TO_MIGRATE])
             ->addFieldToFilter('updated_at', ['lteq' => self::TIME_TO_MIGRATE])
             ->addFieldToFilter('store_id', ['in' => $this->getWebsiteApply()])
             ->addAttributeToFilter('call_subscription_status', 1);
-
+        $customers
+            ->setPageSize($quantity)
+            ->setCurPage(1)
+            ->load();
+        $output->writeln("Customers' SMS marketing synchronization has started");
+        $output->writeln("Check more detail of progress at this file var/log/pos.log");
         foreach ($customers as $customer) {
             try {
                 $this->disableAddressValidation($customer);
                 $customer->setSmsSubscriptionStatus(1);
                 $customer->save();
+                $output->writeln("Done sync customer with ID: " . $customer->getEntityId());
             } catch (\Exception $exception) {
-                $this->logger->error('Error when sync customer with ID', [
-                    'customerID' => $customer->getEntityId(),
-                    'errorMessage' => $exception->getMessage()
-                ]);
+                $output->writeln('Exception when sync customer with ID: ' . $customer->getEntityId());
+                $output->writeln('Exception message: ' . $exception->getMessage());
+            } catch (\Throwable $throwable) {
+                $output->writeln('Throwable when sync customer with ID: ' . $customer->getEntityId());
+                $output->writeln('Throwable message: ' . $throwable->getMessage());
             }
         }
+        $output->writeln("All Process Done");
     }
 
     /**
@@ -95,7 +116,7 @@ class MigrationSMSMarketingAgreement implements DataPatchInterface
     /**
      * @return array
      */
-    protected function getWebsiteApply()
+    private function getWebsiteApply()
     {
         $storeApply = [];
         foreach ($this->storeManager->getStores() as $store) {
@@ -104,21 +125,5 @@ class MigrationSMSMarketingAgreement implements DataPatchInterface
             }
         }
         return $storeApply;
-    }
-
-    /**
-     * @return array|string[]
-     */
-    public static function getDependencies(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array|string[]
-     */
-    public function getAliases(): array
-    {
-        return [];
     }
 }
