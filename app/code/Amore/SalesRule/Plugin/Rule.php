813cd3358f2c6e4c2e9a34d791c128d443f42902
nghiepthan <nghiepthan@amorepacific.com>
2022-04-08 09:28:27 +0700

    [VNMGDC-500]-Update to plugin before

--- app/code/Amore/SalesRule/Plugin/Rule.php
+++ app/code/Amore/SalesRule/Plugin/Rule.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 namespace Amore\SalesRule\Plugin;
 
 use Magento\SalesRule\Model\Rule as SalesRule;
@@ -10,16 +6,15 @@ use Magento\SalesRule\Model\Rule as SalesRule;
 class Rule
 {
     /**
-     * Around plugin needed for existing and new rules alike
+     * Before plugin needed for existing and new rules alike
      * @param SalesRule $subject
-     * @param \Closure $proceed
      * @return SalesRule
      */
-    public function aroundSave(SalesRule $subject, \Closure $proceed)
+    public function beforeSave(SalesRule $subject)
     {
         if (in_array($subject->getSimpleAction(), \Amasty\Promo\Observer\Salesrule\Discount::PROMO_RULES)) {
             $subject->setData(\Magento\SalesRule\Model\Data\Rule::KEY_SIMPLE_FREE_SHIPPING, 0);
         }
-        return $proceed();
+        return [];
     }
 }
