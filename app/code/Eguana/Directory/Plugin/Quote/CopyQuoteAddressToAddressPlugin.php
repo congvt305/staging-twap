6c25524508814d76bebb024544be5b2d2097b37f
binh nguyen <binhnguyen@amorepacific.com>
2022-04-22 08:21:38 +0530

    VNMGDC-513 ward require and inactive free gift

---
+++ app/code/Eguana/Directory/Plugin/Quote/CopyQuoteAddressToAddressPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Eguana\Directory\Plugin\Quote;
+
+use Magento\Quote\Model\Quote\Address;
+
+class CopyQuoteAddressToAddressPlugin
+{
+    /**
+     * @param Address $subject
+     * @param $result
+     * @return mixed
+     */
+    public function afterExportCustomerAddress(Address $subject, $result)
+    {
+        if ($subject->getCityId()) {
+            $result->setCustomAttribute('city_id', $subject->getCityId());
+        }
+        if ($subject->getWardId()) {
+            $result->setCustomAttribute('ward_id', $subject->getWardId());
+        }
+        if ($subject->getWard()) {
+            $result->setCustomAttribute('ward', $subject->getWard());
+        }
+        return $result;
+    }
+}
