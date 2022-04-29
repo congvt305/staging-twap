c8d7f506416a6babdcdb1b5a2ced67a78089d2f7
minh.manh <minh.manh@amorepacific.com>
2022-04-29 10:28:54 +0700

    [VNMDGC-388]refractor function createEinvoice to fix issue can not create e-invoice when get response from SAP

--- app/code/Ecpay/Ecpaypayment/Controller/Invoice/InvoiceController.php
+++ app/code/Ecpay/Ecpaypayment/Controller/Invoice/InvoiceController.php
@@ -13,6 +13,7 @@ namespace Ecpay\Ecpaypayment\Controller\Invoice;
 use Ecpay\Ecpaypayment\Model\Payment as EcpayPaymentModel;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Sales\Model\OrderRepository;
 
 class InvoiceController extends Action
 {
@@ -25,22 +26,42 @@ class InvoiceController extends Action
      */
     private $storeManager;
 
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+
+    /**
+     * @param Context $context
+     * @param EcpayPaymentModel $ecpayPaymentModel
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param OrderRepository $orderRepository
+     */
     public function __construct(
         Context $context,
         EcpayPaymentModel $ecpayPaymentModel,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        OrderRepository $orderRepository
     ) {
         parent::__construct($context);
         $this->ecpayPaymentModel = $ecpayPaymentModel;
         $this->storeManager = $storeManager;
+        $this->orderRepository = $orderRepository;
     }
 
+    /**
+     * Create e-invoice
+     *
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function execute()
     {
         try {
             $orderId = $this->getRequest()->getParam("order_id");
-            $storeId = $this->storeManager->getStore()->getId();
-            $resArr = $this->ecpayPaymentModel->createEInvoice($orderId, $storeId);
+            $order = $this->orderRepository->get($orderId);
+            $resArr = $this->ecpayPaymentModel->createEInvoice($order);
             echo print_r($resArr, true) . "<br/>";
         } catch (\Exception $e) {
             throw $e;
