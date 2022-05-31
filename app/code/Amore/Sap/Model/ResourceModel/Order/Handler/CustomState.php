b1b92bdc1d7d426452fb0e8de46186d2edb0d552
duybui <duybui@amorepacific.com>
2022-05-31 10:36:07 +0700

    VNMGDC-571: update shipping method

--- app/code/Amore/Sap/Model/ResourceModel/Order/Handler/CustomState.php
+++ app/code/Amore/Sap/Model/ResourceModel/Order/Handler/CustomState.php
@@ -15,10 +15,15 @@ class CustomState extends \Magento\Sales\Model\ResourceModel\Order\Handler\State
     public function check(Order $order)
     {
         $shippingMethod = $order->getShippingMethod();
-        if ($shippingMethod == 'gwlogistics_CVS' || $shippingMethod == 'ninjavan_tablerate' &&
+        if ($shippingMethod == 'gwlogistics_CVS' &&
             ($order->getStatus() == 'sap_processing' ||
             $order->getStatus() == 'sap_success' || $order->getStatus() == 'sap_fail' ||
-            $order->getStatus() == 'preparing' || $order->getStatus() == 'shipment_processing')) {
+            $order->getStatus() == 'preparing')) {
+            $order->setState("processing")->setStatus($order->getStatus());
+        } elseif ($shippingMethod == 'ninjavan_tablerate' &&
+            ($order->getStatus() == 'sap_processing' ||
+                $order->getStatus() == 'sap_success' || $order->getStatus() == 'sap_fail' ||
+                $order->getStatus() == 'preparing' || $order->getStatus() == 'shipment_processing')) {
             $order->setState("processing")->setStatus($order->getStatus());
         } else {
             $currentState = $order->getState();
