31601c2ea5015429b9f5953ccbf2b8262712ed7d
minh.manh <minh.manh@amorepacific.com>
2022-08-03 17:22:59 +0700

    [VNMGDC-592]fix error cannot save new customer address

--- app/design/frontend/Laneige/vn/Magento_Customer/web/js/model/customer-addresses.js
+++ app/design/frontend/Laneige/vn/Magento_Customer/web/js/model/customer-addresses.js
@@ -21,17 +21,18 @@ define([
          */
         getAddressItems: function () {
             var items = [],
-                customerData = window.customerData;
+                customerData = window.customerData,
+                i = 1,
+                addressLength = 0;
 
             if (isLoggedIn()) {
                 if (Object.keys(customerData).length) {
+                    addressLength = Object.keys(customerData.addresses).length
                     $.each(customerData.addresses, function (key, item) {
-                        items.push(new Address(item));
-                        //customize here
-                        if (items.length === 2) {
-                            //break the loop to limit 2 line shipping
-                            return false;
+                        if (item.default_shipping || i === addressLength) {
+                            items.push(new Address(item));
                         }
+                        i++;
                     });
                 }
             }
