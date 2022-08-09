e32ab688ab164b940e77727bb51d2a4055ea5ae4
Phat Pham <phamanhphat@amorepacific.com>
2022-08-09 17:05:32 +0700

    VNMGDC-382 disable Yotpo review extension

--- app/etc/config.php
+++ app/etc/config.php
@@ -801,7 +801,7 @@ return [
         'Vertex_RequestLogging' => 0,
         'Vertex_AddressValidation' => 0,
         'Vertex_TaxStaging' => 0,
-        'Yotpo_Yotpo' => 1
+        'Yotpo_Yotpo' => 0
     ],
     'admin_user' => [
         'locale' => [
