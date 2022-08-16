3e67ef3748c2186df3a0922bbe02ada21940fe7e
minh.manh <minh.manh@amorepacific.com>
2022-08-16 11:39:32 +0700

    [VNMGDC-399]change new translate

--- app/code/Eguana/OrderDeliveryMessage/view/frontend/web/js/shipping-validation-mixin.js
+++ app/code/Eguana/OrderDeliveryMessage/view/frontend/web/js/shipping-validation-mixin.js
@@ -4,8 +4,12 @@
 
 define([
     'jquery',
-    'mage/translate'
-], function ($, $t) {
+    'mage/translate',
+    'uiRegistry',
+    'Magento_Customer/js/model/customer',
+    'underscore',
+    'Magento_Checkout/js/model/quote'
+], function ($, $t, registry, customer, _, quote) {
     'use strict';
 
     return function (Component) {
@@ -17,6 +21,22 @@ define([
              * @return {Boolean}
              */
             validateShippingInformation: function () {
+                var messageContainer = registry.get('checkout.errors').messageContainer;
+
+                if (customer.isLoggedIn()
+                    && (
+                        !quote.shippingAddress().lastname ||
+                        !quote.shippingAddress().firstname ||
+                        !quote.shippingAddress().street ||
+                        !quote.shippingAddress().telephone
+                    )
+                ) {
+                    messageContainer.addErrorMessage({
+                        message: $t('Please select delivery method : home delivery / cvs delivery. Thanks')
+                    });
+
+                    return false;
+                }
 
                 var superResult = this._super();
 
