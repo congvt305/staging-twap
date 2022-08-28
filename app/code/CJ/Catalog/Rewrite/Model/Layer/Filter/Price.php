a6211785f1af063f34df50fc4ee64d9bf46cd692
binh nguyen <binhnguyen@amorepacific.com>
2022-08-28 10:31:38 +0530

    VNMGDC-730 fix bug filter price

---
+++ app/code/CJ/Catalog/Rewrite/Model/Layer/Filter/Price.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace CJ\Catalog\Rewrite\Model\Layer\Filter;
+
+
+class Price extends \Magento\CatalogSearch\Model\Layer\Filter\Price
+{
+    /**
+     * @var \Magento\Framework\Pricing\PriceCurrencyInterface
+     */
+    protected $priceCurrency;
+
+    /**
+     * @var \Magento\Catalog\Model\Layer\Filter\DataProvider\Price
+     */
+    protected $dataProvider;
+
+    /**
+     * @param \Magento\Catalog\Model\Layer\Filter\ItemFactory $filterItemFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Catalog\Model\Layer $layer
+     * @param \Magento\Catalog\Model\Layer\Filter\Item\DataBuilder $itemDataBuilder
+     * @param \Magento\Catalog\Model\ResourceModel\Layer\Filter\Price $resource
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Framework\Search\Dynamic\Algorithm $priceAlgorithm
+     * @param \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency
+     * @param \Magento\Catalog\Model\Layer\Filter\Dynamic\AlgorithmFactory $algorithmFactory
+     * @param \Magento\Catalog\Model\Layer\Filter\DataProvider\PriceFactory $dataProviderFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Catalog\Model\Layer\Filter\ItemFactory               $filterItemFactory,
+        \Magento\Store\Model\StoreManagerInterface                    $storeManager,
+        \Magento\Catalog\Model\Layer                                  $layer,
+        \Magento\Catalog\Model\Layer\Filter\Item\DataBuilder          $itemDataBuilder,
+        \Magento\Catalog\Model\ResourceModel\Layer\Filter\Price       $resource,
+        \Magento\Customer\Model\Session                               $customerSession,
+        \Magento\Framework\Search\Dynamic\Algorithm                   $priceAlgorithm,
+        \Magento\Framework\Pricing\PriceCurrencyInterface             $priceCurrency,
+        \Magento\Catalog\Model\Layer\Filter\Dynamic\AlgorithmFactory  $algorithmFactory,
+        \Magento\Catalog\Model\Layer\Filter\DataProvider\PriceFactory $dataProviderFactory,
+        array                                                         $data = [])
+    {
+        $this->priceCurrency = $priceCurrency;
+        parent::__construct(
+            $filterItemFactory,
+            $storeManager,
+            $layer, $itemDataBuilder,
+            $resource,
+            $customerSession,
+            $priceAlgorithm,
+            $priceCurrency,
+            $algorithmFactory,
+            $dataProviderFactory,
+            $data
+        );
+        $this->dataProvider = $dataProviderFactory->create(['layer' => $this->getLayer()]);
+    }
+
+    /**
+     * @param $fromPrice
+     * @param $toPrice
+     * @param $isLast
+     * @return float|\Magento\Framework\Phrase
+     */
+    protected function _renderRangeLabel($fromPrice, $toPrice, $isLast = false)
+    {
+        $fromPrice = empty($fromPrice) ? 0 : $fromPrice * $this->getCurrencyRate();
+        $toPrice = empty($toPrice) ? 0 : $toPrice * $this->getCurrencyRate();
+
+        $formattedFromPrice = $this->priceCurrency->format($fromPrice);
+        if ($isLast) {
+            return __('%1 and above', $formattedFromPrice);
+        } elseif ($fromPrice == $toPrice && $this->dataProvider->getOnePriceIntervalValue()) {
+            return $formattedFromPrice;
+        } else {
+            if ($fromPrice != $toPrice) {
+                $toPrice -= .01;
+            }
+
+            return __('%1 - %2', $formattedFromPrice, $this->priceCurrency->format($toPrice));
+        }
+    }
+}
