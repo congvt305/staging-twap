19d8604d33b30d15304738db8bac0bc0b758eae2
Phat Pham <phamanhphat@amorepacific.com>
2022-09-19 16:09:05 +0700

    hotfix increase curl timeout from 30s into 60s

--- app/code/Amore/Base/Model/BaseRequest.php
+++ app/code/Amore/Base/Model/BaseRequest.php
@@ -62,7 +62,7 @@ abstract class BaseRequest
             $requestData['salOffCd'] = $this->middlewareHelper->getSalesOfficeCode($scope, $websiteId);
 
         }
-        $this->curl->setTimeout(30);
+        $this->curl->setTimeout(60);
         $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/request.log');
         $logger = new \Zend_Log();
         $logger->addWriter($writer);
