04f0a81e9359556f662286b724c4ea6571627e15
Hoang Phan <hoangphan@amorepacific.com>
2022-11-29 05:58:57 -0500

    ITO0016-43 Fix coupon component

--- app/code/CJ/Checkout/view/frontend/requirejs-config.js
+++ app/code/CJ/Checkout/view/frontend/requirejs-config.js
@@ -1,20 +1,8 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Checkout/js/view/minicart' : {
-                'CJ_Checkout/js/minicart-content-mixin': true
-            },
-            'Magento_Checkout/js/view/payment': {
-                'CJ_Checkout/js/view/payment-mixin': true
-            },
-            'Magento_Ui/js/lib/validation/validator': {
-                'CJ_Checkout/js/validator-mixin': true
-            },
-            'Magento_Checkout/js/model/checkout-data-resolver': {
-                'CJ_Checkout/js/model/checkout-data-resolver-mixin': true
-            },
-            'Magento_Checkout/js/checkout-data': {
-                'CJ_Checkout/js/checkout-data-mixin': true
+            'Amasty_Coupons/js/view/abstract-discount' : {
+                'CJ_Coupons/js/view/abstract-discount-mixin': true
             }
         }
     }
