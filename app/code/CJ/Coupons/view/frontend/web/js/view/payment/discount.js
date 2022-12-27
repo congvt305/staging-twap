2f553574fff0a750c946d0ac6ed0b16f543369a3
Hoang Phan <hoangphan@amorepacific.com>
2022-12-27 16:13:26 +0700

    ITO0309-49 Fix issue when apply the wrong coupon on MY LNG

--- app/code/CJ/Coupons/view/frontend/web/js/view/payment/discount.js
+++ app/code/CJ/Coupons/view/frontend/web/js/view/payment/discount.js
@@ -10,14 +10,17 @@ define(
         'uiComponent',
         'Magento_Checkout/js/model/quote',
         'Magento_SalesRule/js/model/payment/discount-messages',
+        'Amasty_Coupons/js/action/apply-coupon-codes',
         'Amasty_Coupons/js/action/set-coupon-code',
         'Magento_SalesRule/js/action/cancel-coupon',
         'Magento_SalesRule/js/model/coupon',
+        'Amasty_Coupons/js/model/coupon',
         'Magento_Ui/js/modal/modal',
         'text!CJ_Coupons/template/modal/modal-popup.html',
+        'Amasty_Coupons/js/model/abstract-apply-response-processor',
         'domReady!'
     ],
-    function ($, ko, _, $t, Component, quote, messageContainer, setCouponCodeAction, cancelCouponAction,couponAction, modal, popupTpl) {
+    function ($, ko, _, $t, Component, quote, messageContainer, setCouponCodesAction, setCouponCodeAction, cancelCouponAction,couponAction, couponModel, modal, popupTpl, responseProcessor) {
         'use strict';
 
         var totals = quote.getTotals(),
@@ -101,6 +104,14 @@ define(
         var message = $t('Your coupon was successfully applied'),
             messageError = $t('Your coupon code is not valid'),
             messageDelete = $t('Coupon code was removed');
+
+
+        /*
+         * Adding new attributes that only exist on latest version of Amasty Coupons
+         **/
+        var couponsArray = couponModel.couponsArray;
+
+
         return Component.extend({
 
             defaults: {
@@ -112,6 +123,8 @@ define(
 
             couponCode: couponCode,
             fakeCouponCode: fakeCouponCode,
+            errorMessage: messageError,
+            successMessage: message,
 
             /**
              * Applied flag
@@ -133,6 +146,22 @@ define(
              * check should show couponlist
              */
             canViewCouponList: ko.observable(canViewCouponList),
+
+            /*
+            * Adding new attributes that only exist on latest version of Amasty Coupons
+            **/
+            couponsArray: couponsArray,
+            inputCode: '',
+            responseProcessor: responseProcessor,
+            initObservable: function () {
+                this._super();
+
+                this.observe(['inputCode']);
+
+                return this;
+            },
+            /*End adding new attributes*/
+
 
             createPopupWebsite: function() {
                 if (websiteCode == 'tw_lageige_website') {
@@ -214,7 +243,7 @@ define(
              * Coupon code application procedure
              */
             apply: function() {
-                if (this.validate()) {
+              /*  if (this.validate()) {
                     isLoading(true);
                     var newDiscountCode =  this.fakeCouponCode();
                     var code = [];
@@ -238,7 +267,42 @@ define(
                     }.bind(this)).always(function(){
                         isLoading(false);
                     });
+                }*/
+
+                var codes = [];
+
+                if (this.validate()) {
+                    this.isLoading(true);
+                    this.inputCode(this.fakeCouponCode());
+                    codes = codes.concat(this.couponsArray())
+                        .concat(couponModel.renderCoupons(this.inputCode()));
+
+                    setCouponCodesAction(codes, this.responseProcessor)
+                        .done(function () {
+                            this.handleErrorMessages();
+                            this.inputCode(this.responseProcessor.errorCoupons.join(', '));
+                            let couponCodes = [];
+                            couponCodes = couponCodes
+                                .concat(this.responseProcessor.appliedCoupons)
+                                .concat(this.responseProcessor.notChangedCoupons);
+                            if (couponCodes.length > 0) {
+                                this.isApplied(true);
+                                this.couponCode(couponCodes.join(', '));
+                                let messages = this.getChild('errors');
+                                messages.messageContainer.clear();
+                                messages.messageContainer.addSuccessMessage({
+                                    'message': this.successMessage
+                                });
+                            }
+
+                            $('.totals.discount .title').removeClass('negative');
+                            window.location.reload();
+                        }.bind(this))
+                        .always(function () {
+                            this.isLoading(false);
+                        }.bind(this));
                 }
+
             },
 
             /**
@@ -260,6 +324,19 @@ define(
             validate: function() {
                 var form = '#discount-form';
                 return $(form).validation() && $(form).validation('isValid');
+            },
+
+            /**
+             * @returns {void}
+             */
+            handleErrorMessages: function () {
+                var messages = this.getChild('errors');
+
+                messages.messageContainer.clear();
+
+                _.each(responseProcessor.errorCoupons, function (code) {
+                    messages.messageContainer.errorMessages.push(code + ' ' + this.errorMessage);
+                }, this);
             },
 
             /**
