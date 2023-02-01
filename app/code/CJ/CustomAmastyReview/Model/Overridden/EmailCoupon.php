e4f1cb82cf0bec0b5e275cb2ad42d9f5ebc8a907
Hoang Phan <hoangphan@amorepacific.com>
2023-02-01 13:48:31 +0700

    ITO0035-40 Change to use scope Store instead of Website

---
+++ app/code/CJ/CustomAmastyReview/Model/Overridden/EmailCoupon.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace CJ\CustomAmastyReview\Model\Overridden;
+
+use Amasty\AdvancedReview\Helper\Config;
+use Amasty\AdvancedReview\Model\Email\CouponDataProvider;
+use Amasty\AdvancedReview\Model\Email\SalesRuleProvider;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\SalesRule\Api\CouponRepositoryInterface;
+use Magento\SalesRule\Api\RuleRepositoryInterface;
+use Magento\SalesRule\Model\CouponFactory;
+use Magento\SalesRule\Model\ResourceModel\Rule\Collection;
+use Magento\SalesRule\Model\ResourceModel\Rule\CollectionFactory;
+use Magento\SalesRule\Model\Rule;
+use Magento\Store\Api\Data\WebsiteInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class EmailCoupon
+ */
+class EmailCoupon extends \Amasty\AdvancedReview\Model\Email\Coupon
+{
+    /**
+     * @var bool
+     */
+    protected $sendCoupon = false;
+
+    /**
+     * @var DateTime
+     */
+    protected $date;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $ruleCollectionFactory;
+
+    /**
+     * @var RuleRepositoryInterface
+     */
+    protected $ruleRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var CouponFactory
+     */
+    protected $couponFactory;
+
+    /**
+     * @var CouponRepositoryInterface
+     */
+    protected $couponRepository;
+
+    /**
+     * @var SalesRuleProvider
+     */
+    protected $salesRuleProvider;
+
+    /**
+     * @var \CJ\CustomAmastyReview\Helper\Config
+     */
+    protected $helper;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param Config $configHelper
+     * @param DateTime $date
+     * @param CollectionFactory $ruleCollectionFactory
+     * @param LoggerInterface $logger
+     * @param RuleRepositoryInterface $ruleRepository
+     * @param CouponFactory $couponFactory
+     * @param CouponRepositoryInterface $couponRepository
+     * @param SalesRuleProvider $salesRuleProvider
+     * @param \CJ\CustomAmastyReview\Helper\Config $helper
+     */
+    public function __construct(
+        Config $configHelper,
+        DateTime $date,
+        CollectionFactory $ruleCollectionFactory,
+        LoggerInterface $logger,
+        RuleRepositoryInterface $ruleRepository,
+        CouponFactory $couponFactory,
+        CouponRepositoryInterface $couponRepository,
+        SalesRuleProvider $salesRuleProvider,
+        \CJ\CustomAmastyReview\Helper\Config $helper,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->helper = $helper;
+        $this->storeManager = $storeManager;
+        parent::__construct($configHelper, $date, $ruleCollectionFactory, $logger, $ruleRepository, $couponFactory, $couponRepository, $salesRuleProvider);
+        $this->date = $date;
+        $this->ruleCollectionFactory = $ruleCollectionFactory;
+        $this->logger = $logger;
+        $this->ruleRepository = $ruleRepository;
+        $this->couponFactory = $couponFactory;
+        $this->couponRepository = $couponRepository;
+        $this->salesRuleProvider = $salesRuleProvider;
+    }
+
+    /**
+     * @param   string $couponCode
+     * @param   int $days
+     * @return  string
+     */
+    private function getCouponText(string $couponCode, int $days): string
+    {
+        $daysMessage = $this->getDaysMessage($days);
+        return sprintf(
+            '<p class="amcomment">%s</p><p class="am-coupon-container">%s<span class="coupon">%s</span> (%s).</p>',
+            __('It will take only a few minutes, just click the \'Leave a review\' button below.'),
+            __(
+                'To make the process more pleasant we are happy to grant you a discount coupon code,'
+                . ' which can already be used for your next purchase. Here it is: '
+            ),
+            $couponCode,
+            $daysMessage
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function getNoCouponText(): string
+    {
+        return (string) __('It will take only a few minutes, just click the \'Leave a review\' button below.');
+    }
+
+    /**
+     * @return Collection
+     */
+    private function getExpiredRuleCollection(): Collection
+    {
+        /** @var Collection $collection */
+        $collection = $this->ruleCollectionFactory->create();
+        $collection->addFieldToFilter('coupon_type', ['eq' => Rule::COUPON_TYPE_SPECIFIC]);
+        $collection->addFieldToFilter(
+            ['name', 'name'],
+            [['like' => '%@%.%'], ['like' => CouponDataProvider::$baseRuleName . ' %']]
+        );
+        $collection->addFieldToFilter('to_date', ['lt' => $this->date->date('Y-m-d')]);
+        return $collection;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCouponMessage(WebsiteInterface $website): string
+    {
+        $storeId = $this->_getStoreIdByWebsite($website);
+        if ($this->helper->isAllowCoupons($storeId)) {
+            if ($this->helper->isNeedReview($storeId)) {
+                $message = $this->getReviewText();
+            } else {
+                $days = (int)$this->helper->getModuleConfig('coupons/coupon_days', $storeId);
+                $couponCode = $this->generateCoupon($website);
+                $message = $this->getCouponText($couponCode, $days);
+
+                if ($couponCode) {
+                    $this->sendCoupon = true;
+                }
+            }
+        } else {
+            $message = $this->getNoCouponText();
+        }
+
+        return $message;
+    }
+
+    /**
+     * @param WebsiteInterface $website
+     * @return int
+     */
+    private function _getStoreIdByWebsite(WebsiteInterface $website): int
+    {
+        $storeId = null;
+        foreach ($this->storeManager->getStores() as $store) {
+            if ($website->getId() === $store->getWebsiteId()) {
+                $storeId = $store->getId();
+            }
+        }
+        return $storeId;
+    }
+}
