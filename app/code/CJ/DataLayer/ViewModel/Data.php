b0c816ab6364199becb87b0269c12f952b87736a
Hoang Phan <hoangphan@amorepacific.com>
2023-02-23 18:05:37 +0700

    BRANDSR-2485 Add data to datalayer on order success

--- app/code/CJ/DataLayer/ViewModel/Data.php
+++ app/code/CJ/DataLayer/ViewModel/Data.php
@@ -1,7 +1,9 @@
 <?php
-
+declare(strict_types=1);
 namespace CJ\DataLayer\ViewModel;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class Data implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
@@ -10,21 +12,47 @@ class Data implements \Magento\Framework\View\Element\Block\ArgumentInterface
      */
     protected $scopeConfig;
 
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
 
+    /**
+     * @var \Magento\Directory\Model\CurrencyFactory
+     */
+    protected $currencyFactory;
 
     const XML_PATH_MIN_QTY_ALLOW = 'cataloginventory/item_options/min_sale_qty';
 
     /**
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
     public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory
     ) {
         $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+        $this->currencyFactory = $currencyFactory;
     }
 
+    /**
+     * @return int
+     */
     public function getMinQtyAllow(): int
     {
         return (int)$this->scopeConfig->getValue(self::XML_PATH_MIN_QTY_ALLOW);
+    }
+
+    /**
+     * @return string
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    public function getCurrencyCode(): string
+    {
+        return $this->storeManager->getStore()->getCurrentCurrency()->getCode();
     }
 }
