457c9e87bbf4b17be0bcef5441b466f530d7caf0
Phat Pham <phamanhphat@amorepacific.com>
2023-05-16 15:10:55 +0700

    ITO0016-163 insert an additional script within the GTM tag

--- pub/qg-sw.059208f4dab3fe8375c9.js
+++
@@ -1,2 +0,0 @@
-var appId = "059208f4dab3fe8375c9";
-importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
--- pub/qg-sw.e6b7b01e1cd059868135.js
+++
@@ -1,2 +0,0 @@
-var appId = "e6b7b01e1cd059868135";
-importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
