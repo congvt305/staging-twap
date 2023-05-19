249e50cffa8a51fec2f5cf766e6a317e55ab0d01
Hoang Phan <hoangphan@amorepacific.com>
2023-05-19 14:04:10 +0700

    BRANDSR-4339 Fix feedback

--- app/code/Amore/CustomerRegistration/Plugin/CreateCustomer.php
+++ app/code/Amore/CustomerRegistration/Plugin/CreateCustomer.php
@@ -117,10 +117,12 @@ class CreateCustomer
         $websiteIds = $this->getWebsiteIds();
         asort($websiteIds);
         foreach ($websiteIds as $websiteId) {
-            $officeSaleCode = $this->configHelper->getOfficeSalesCode($websiteId);
-            if ($officeSaleCode == $salOffCd) {
-                $customerWebsiteId = $websiteId;
-                break;
+            if ($websiteId) {
+                $officeSaleCode = $this->configHelper->getOfficeSalesCode($websiteId);
+                if ($officeSaleCode == $salOffCd) {
+                    $customerWebsiteId = $websiteId;
+                    break;
+                }
             }
         }
         return $customerWebsiteId;
