8ae31f036cb05d880bd1ddd687d8228cbb7b0532
Minh Manh <minh.manh@amorepacific.com>
2023-06-15 17:11:52 +0700

    [BRANDSR-4214]fix link URL when send multiple email reminder

--- app/code/CJ/CustomAmastyReview/Block/Overridden/ReminderEmailContent.php
+++ app/code/CJ/CustomAmastyReview/Block/Overridden/ReminderEmailContent.php
@@ -9,6 +9,7 @@ use Magento\Framework\Filter\Input\MaliciousCode;
 use Magento\Framework\View\Element\Template;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\UrlRewrite\Model\UrlFinderInterface;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
 
 /**
  * Class ReminderEmailContent
@@ -108,5 +109,69 @@ class ReminderEmailContent extends \Amasty\AdvancedReview\Block\Email\ReminderEm
         $products = $this->productRepository->getList($searchCriteria)->getItems();
 
         return $products;
+    }
+
+    /**
+     * Override to custom get url product
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     *
+     * @return string
+     */
+    public function getProductUrl(\Magento\Catalog\Model\Product $product)
+    {
+        $params = [];
+        $params['_nosid'] = true;
+        $routePath = '';
+        $routeParams = $params;
+
+        $storeId = $product->getStoreId();
+
+        $categoryId = null;
+
+        if ($product->hasUrlDataObject()) {
+            $requestPath = $product->getUrlDataObject()->getUrlRewrite();
+            $routeParams['_scope'] = $product->getUrlDataObject()->getStoreId();
+        } else {
+            $requestPath = $product->getRequestPath();
+            if (empty($requestPath) && $requestPath !== false) {
+                $filterData = [
+                    UrlRewrite::ENTITY_ID => $product->getId(),
+                    UrlRewrite::ENTITY_TYPE => \Magento\CatalogUrlRewrite\Model\ProductUrlRewriteGenerator::ENTITY_TYPE,
+                    UrlRewrite::STORE_ID => $storeId,
+                ];
+                if ($categoryId) {
+                    $filterData[UrlRewrite::METADATA]['category_id'] = $categoryId;
+                }
+                $rewrite = $this->urlFinder->findOneByData($filterData);
+                if ($rewrite) {
+                    $requestPath = $rewrite->getRequestPath();
+                    $product->setRequestPath($requestPath);
+                } else {
+                    $product->setRequestPath(false);
+                }
+            }
+        }
+
+        if (!empty($requestPath)) {
+            $routeParams['_direct'] = $requestPath;
+        } else {
+            $routePath = 'catalog/product/view';
+            $routeParams['id'] = $product->getId();
+            $routeParams['s'] = $product->getUrlKey();
+            if ($categoryId) {
+                $routeParams['category'] = $categoryId;
+            }
+        }
+
+        // reset cached URL instance GET query params
+        if (!isset($routeParams['_query'])) {
+            $routeParams['_query'] = [];
+        }
+        //custom here
+        //set store for route param to avoid get the same cache key for same product, same url but diffirent site when get url in send multiple email at the same time
+        $routeParams['storeId'] = $storeId;
+        //end custom
+        return $this->url->setScope($storeId)->getUrl($routePath, $routeParams);
     }
 }
