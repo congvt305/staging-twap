1c425fc36d06e584322931f2727daa3b5b92a0d3
Hoang Phan <hoangphan@amorepacific.com>
2023-06-27 16:49:41 +0700

    BRANDSR-5046 Add config

---
+++ app/code/Eguana/SocialLogin/Model/LineBanner/Options.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Eguana\SocialLogin\Model\LineBanner;
+
+use Magento\Cms\Model\ResourceModel\Block\CollectionFactory;
+
+class Options implements  \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Block collection factory
+     *
+     * @var CollectionFactory
+     */
+    protected $_blockCollectionFactory;
+
+    /**
+     * @param CollectionFactory $_blockCollectionFactory
+     */
+    public function __construct(CollectionFactory $_blockCollectionFactory)
+    {
+        $this->_blockCollectionFactory = $_blockCollectionFactory;
+    }
+
+
+    public function toOptionArray()
+    {
+        $options = $this->_blockCollectionFactory->create()->load()->toOptionArray();
+        array_unshift($options, ['value' => '', 'label' => __('Please select a static block.')]);
+        return $options;
+    }
+
+}
