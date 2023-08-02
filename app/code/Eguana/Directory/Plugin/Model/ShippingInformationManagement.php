26ca660f43bae6ea79750895b30a4fc198a1e714
Minh Manh <minh.manh@amorepacific.com>
2023-08-02 15:04:24 +0700

    [BRANDSR-5435]revert validation for MY LNG and VN LNG

--- app/code/Eguana/Directory/Plugin/Model/ShippingInformationManagement.php
+++ app/code/Eguana/Directory/Plugin/Model/ShippingInformationManagement.php
@@ -11,6 +11,13 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class ShippingInformationManagement
 {
+    const TW_STORE_CODE = [
+        'tw_laneige',
+        'default'
+    ];
+
+    const MY_STORE_CODE = 'my_laneige';
+
     /**
      * @var StoreManagerInterface
      */
@@ -51,13 +58,27 @@ class ShippingInformationManagement
     ) {
         $storeCode = $this->storeManager->getStore()->getCode();
         $address = $addressInformation->getShippingAddress();
+        $telephone = $address->getTelephone();
         if ($storeCode === $this->storeCode) {
             if ($this->amastyConfig->isEnabled()) {
                 $this->addAddressForAmastyOnePageCheckout($addressInformation);
             } else {
                 $this->validateAddress($address);
             }
+            if (!preg_match('/^[0]{1}\d{9}$/', $telephone)) {
+                throw new InputException(
+                    __("Please enter exactly proper mobile number. Start with 0 and 10 digit.")
+                );
+            }
         }
+        if (in_array($storeCode, self::TW_STORE_CODE)) {
+            if (!preg_match('/^[0]{1}[9]{1}\d{8}$/', $telephone)) {
+                throw new InputException(
+                    __("Please enter exactly proper mobile number. Start with 09 and 10 digit.")
+                );
+            }
+        }
+
         if (!$address->getLastname() || !$address->getFirstname() || !$address->getStreet() || !$address->getTelephone()) {
             throw new InputException(
                 __("Please select delivery method : home delivery / cvs delivery. Thanks")
