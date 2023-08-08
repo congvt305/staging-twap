7b52662211652e62b7bbb6b99d0465c746476138
Minh Manh <minh.manh@amorepacific.com>
2023-08-08 09:02:55 +0700

    [BRANDSR-5233]fix error cannot calculate in MY LNG don't use point

--- app/code/Amore/Sap/Model/SapOrder/SapOrderConfirmData.php
+++ app/code/Amore/Sap/Model/SapOrder/SapOrderConfirmData.php
@@ -334,7 +334,9 @@ class SapOrderConfirmData extends AbstractSapOrder
                 }
                 if ($this->rewardData->isEnableShowListOptionRewardPoint($storeId)) {
                     $listOptions = $this->rewardData->getListOptionRewardPoint($storeId);
-                    $mileageUsedAmount = $listOptions[$rewardPoints] ?? 0;
+                    if ($rewardPoints) {
+                        $mileageUsedAmount = $listOptions[$rewardPoints] ?? 0;
+                    }
                 } else {
                     $mileageUsedAmount = $rewardPoints / $spendingRate;
                 }
@@ -583,8 +585,10 @@ class SapOrderConfirmData extends AbstractSapOrder
             }
             if ($this->rewardData->isEnableShowListOptionRewardPoint($storeId)) {
                 $listOptions = $this->rewardData->getListOptionRewardPoint($storeId);
-                $mileageUsedAmount = $listOptions[$rewardPoints] ?? 0;
-                $spendingRate = $rewardPoints / $mileageUsedAmount;
+                if ($rewardPoints) {
+                    $mileageUsedAmount = $listOptions[$rewardPoints] ?? 0;
+                    $spendingRate = $rewardPoints / $mileageUsedAmount;
+                }
             } else {
                 $mileageUsedAmount = $rewardPoints / $spendingRate;
             }
