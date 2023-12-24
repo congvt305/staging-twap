a907163921c5bf6ad0e8199a4d566ba7ee4e4f63
nghiepthan <nghiepthan@amorepacific.com>
2023-12-24 17:59:18 +0700

    [TWLSAPT-306]_fixing error deploy after cherry-pick code

--- app/code/Sapt/Customer/Block/Membership.php
+++ app/code/Sapt/Customer/Block/Membership.php
@@ -14,7 +14,7 @@ use CJ\CouponCustomer\Helper\Data as CouponHelper;
 use Amore\PointsIntegration\Model\CustomerPointsSearch;
 use Amore\PointsIntegration\Block\Points\Index as PointsIndex;
 
-class Membership extends \CJ\Review\Block\Top\Info
+class Membership extends Template
 {
     /**
      * @var Session
@@ -35,36 +35,116 @@ class Membership extends \CJ\Review\Block\Top\Info
      * @var PointsIndex
      */
     private $pointsIndex;
+
+    protected $_orderCustomerCollection;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $_orderCollectionFactory;
+
+    /**
+     * @var CouponHelper
+     */
+    protected $couponHelper;
+
+    /**
+     * @var Json
+     */
+    protected $json;
+
+    /**
+     * @param Template\Context $context
+     * @param CollectionFactory $orderCollectionFactory
+     * @param Session $customerSession
+     * @param CouponHelper $couponHelper
+     * @param Json $json
+     * @param array $data
+     */
     public function __construct(
-        Template\Context $context,
-        Session $customerSession,
-        GroupRepositoryInterface $groupRepository,
+        Template\Context  $context,
         CollectionFactory $orderCollectionFactory,
+        Session           $customerSession,
         CouponHelper      $couponHelper,
         Json              $json,
+        GroupRepositoryInterface  $groupRepository,
         CustomerPointsSearch $customerPointsSearch,
-        PointsIndex $pointsIndex,
-        array $data = []
+        PointsIndex  $pointsIndex,
+        array             $data = []
     ) {
+        parent::__construct($context, $data);
         $this->customerSession = $customerSession;
         $this->groupRepository = $groupRepository;
         $this->_orderCollectionFactory = $orderCollectionFactory;
-        $this->couponHelper = $couponHelper;
         $this->json = $json;
-        $this->customerPointsSearch = $customerPointsSearch;
         $this->pointsIndex = $pointsIndex;
-        parent::__construct(
-            $context,
-            $orderCollectionFactory,
-            $customerSession,
-            $couponHelper,
-            $json,
-            $customerPointsSearch,
-            $data
-        );
+        $this->couponHelper = $couponHelper;
+        $this->customerPointsSearch = $customerPointsSearch;
     }
 
     /**
+     * @return \Magento\Customer\Model\Customer
+     */
+    public function getCustomer(): \Magento\Customer\Model\Customer
+    {
+        return $this->customerSession->getCustomer();
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountCoupon(): int
+    {
+        return $this->couponHelper->getCustomerAvailableCoupons()->getSize();
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountOrderReviewAvailable(): int
+    {
+        $orderCollection = $this->getOrderCollection();
+        $orderCollection->addFieldToFilter('reviewed', 0);
+        $orderCollection->addFieldToFilter('status', 'complete');
+        return $orderCollection->getSize();
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountOrderReviewed(): int
+    {
+        $orderCollection = $this->getOrderCollection();
+        $orderCollection->addFieldToFilter('reviewed', 1);
+        return $orderCollection->getSize();
+    }
+
+    /**
+     * @return \Magento\Sales\Model\ResourceModel\Order\Collection
+     */
+    private function getOrderCollection(): \Magento\Sales\Model\ResourceModel\Order\Collection
+    {
+        return $this->_orderCollectionFactory->create()
+            ->addAttributeToSelect('status')
+            ->addFieldToFilter('customer_id', $this->getCustomer()->getId());
+    }
+
+    /**
+     * @return int
+     */
+    public function getPoint(): int
+    {
+        $customer = $this->getCustomer();
+        $customerPointsInfo = $this->customerPointsSearch->getMemberSearchResult($customer->getId(), $customer->getWebsiteId());
+        if(!$customerPointsInfo)
+        {
+            return 0;
+        }else{
+            return (int) $customerPointsInfo['data']['availablePoint'] ?? 0;
+        }
+    }
+
+/**
      * @param $groupId
      * @return GroupInterface|string
      */
@@ -76,16 +156,6 @@ class Membership extends \CJ\Review\Block\Top\Info
         } catch (LocalizedException $e) {
         }
         return '';
-    }
-
-    /**
-     * @return \Magento\Sales\Model\ResourceModel\Order\Collection
-     */
-    private function getOrderCollection(): \Magento\Sales\Model\ResourceModel\Order\Collection
-    {
-        return $this->_orderCollectionFactory->create()
-            ->addAttributeToSelect('status')
-            ->addFieldToFilter('customer_id', $this->getCustomer()->getId());
     }
 
     /**
