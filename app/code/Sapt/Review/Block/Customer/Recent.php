ceeedc3a17212c7f009e4d7c903f36b38d1ea20e
Minh Manh <minh.manh@amorepacific.com>
2023-12-28 10:13:02 +0700

    [BRANDSR-7773]fix error reviewid on null for site TW SWS

--- app/code/Sapt/Review/Block/Customer/Recent.php
+++ app/code/Sapt/Review/Block/Customer/Recent.php
@@ -1,47 +1,27 @@
 <?php
-/**
- * Copyright ©  All rights reserved.
- * See COPYING.txt for license details.
- */
 declare(strict_types=1);
 
 namespace Sapt\Review\Block\Customer;
 
-use Magento\Customer\Api\AccountManagementInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Framework\App\ObjectManager;
-use Magento\Review\Helper\Data as ReviewHelper;
-
-class Recent extends ListCustomer
+class Recent extends \Magento\Review\Block\Customer\Recent
 {
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Review\Model\ResourceModel\Review\Product\CollectionFactory $collectionFactory
+     * @param \Magento\Customer\Helper\Session\CurrentCustomer $currentCustomer
+     * @param array $data
+     */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
-        CustomerRepositoryInterface $customerRepository,
-        AccountManagementInterface $customerAccountManagement,
         \Magento\Review\Model\ResourceModel\Review\Product\CollectionFactory $collectionFactory,
         \Magento\Customer\Helper\Session\CurrentCustomer $currentCustomer,
-        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
-        \Magento\Sales\Model\Order\Config $orderConfig,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        array $data = [],
-        ?ReviewHelper $reviewHelper = null
+        array $data = []
     ) {
-        $data['reviewHelper'] = $reviewHelper ?? ObjectManager::getInstance()->get(ReviewHelper::class);
         parent::__construct(
             $context,
-            $customerSession,
-            $subscriberFactory,
-            $customerRepository,
-            $customerAccountManagement,
             $collectionFactory,
             $currentCustomer,
-            $orderCollectionFactory,
-            $orderConfig,
-            $productRepository,
-            $data,
-            $reviewHelper
+            $data
         );
     }
 
