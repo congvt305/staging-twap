a7553fa60b26558d4b4eda44f2fb58e030af0803
Minh Manh <minh.manh@amorepacific.com>
2023-12-28 10:42:12 +0700

    [BRANDSR-7775]check condition set telephone

--- app/code/CJ/AmastyCheckoutCore/Block/Onepage/MobileNumberProcessor.php
+++ app/code/CJ/AmastyCheckoutCore/Block/Onepage/MobileNumberProcessor.php
@@ -9,6 +9,7 @@ use Magento\Customer\Model\Session as CustomerSession;
 
 class MobileNumberProcessor implements LayoutProcessorInterface
 {
+    const NEW_THEME_TW_LNG_PATH = 'AmorePacific/sapt_tw_laneige';
     /**
      * @var CustomerSession
      */
@@ -20,16 +21,23 @@ class MobileNumberProcessor implements LayoutProcessorInterface
     protected $customerRepository;
 
     /**
+     * @var \Magento\Framework\View\DesignInterface
+     */
+    private $design;
+
+    /**
      * @param CustomerSession $session
      * @param CustomerRepositoryInterface $customerRepository
      */
     public function __construct(
         CustomerSession             $session,
-        CustomerRepositoryInterface $customerRepository
+        CustomerRepositoryInterface $customerRepository,
+        \Magento\Framework\View\DesignInterface $design
     )
     {
         $this->customerSession = $session;
         $this->customerRepository = $customerRepository;
+        $this->design = $design;
     }
 
     /**
@@ -40,25 +48,27 @@ class MobileNumberProcessor implements LayoutProcessorInterface
      */
     public function process($jsLayout): array
     {
-        if ($this->customerSession->isLoggedIn()) {
-            $customerId = $this->customerSession->getCustomer()->getId();
-            $customer = $this->customerRepository->getById($customerId);
-            if (isset($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"])) {
-                $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"]["value"] = $customer->getCustomAttribute('mobile_number')->getValue();
-            }
-        }
-
-        $telephoneConfig = $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"];
-        $telephoneConfig["component"] = 'CJ_AmastyCheckoutCore/js/telephone';
-        $telephoneConfig['config']['elementTmpl'] = 'CJ_AmastyCheckoutCore/telephone';
-        $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"] = $telephoneConfig;
-
-        if(isset($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'])) {
-            foreach ($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'] as $key => $value) {
-                if (empty($value['value'])) {
-                    continue;
+        if ($this->design->getDesignTheme()->getThemePath() == self::NEW_THEME_TW_LNG_PATH) {
+            if ($this->customerSession->isLoggedIn()) {
+                $customerId = $this->customerSession->getCustomer()->getId();
+                $customer = $this->customerRepository->getById($customerId);
+                if (isset($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"])) {
+                    $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"]["value"] = $customer->getCustomAttribute('mobile_number')->getValue();
                 }
-                $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'][$key]['label'] = __($value['label']);
+            }
+
+            $telephoneConfig = $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"];
+            $telephoneConfig["component"] = 'CJ_AmastyCheckoutCore/js/telephone';
+            $telephoneConfig['config']['elementTmpl'] = 'CJ_AmastyCheckoutCore/telephone';
+            $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]["telephone"] = $telephoneConfig;
+
+            if(isset($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'])) {
+                foreach ($jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'] as $key => $value) {
+                    if (empty($value['value'])) {
+                        continue;
+                    }
+                    $jsLayout["components"]["checkout"]["children"]["steps"]["children"]["shipping-step"]["children"]["shippingAddress"]["children"]["shipping-address-fieldset"]["children"]['city_id']['options'][$key]['label'] = __($value['label']);
+                }
             }
         }
         return $jsLayout;
