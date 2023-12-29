2f17671f1f9020b95d0c102af0a0c9b37aa2d455
Minh Manh <minh.manh@amorepacific.com>
2023-12-29 13:03:43 +0700

    [TWLSAPT-306]fix issue cannot go to checkout page

--- app/code/Eguana/GWLogistics/Plugin/Quote/ShippingAddressManagementPlugin.php
+++ app/code/Eguana/GWLogistics/Plugin/Quote/ShippingAddressManagementPlugin.php
@@ -13,6 +13,8 @@ use Magento\Quote\Api\Data\AddressInterface;
 
 class ShippingAddressManagementPlugin
 {
+    const NEW_THEME_TW_LNG_PATH = 'AmorePacific/sapt_tw_laneige';
+
     /**
      * @var \Eguana\GWLogistics\Api\QuoteCvsLocationRepositoryInterface
      */
@@ -27,6 +29,11 @@ class ShippingAddressManagementPlugin
     private $logger;
 
     /**
+     * @var \Magento\Framework\View\DesignInterface
+     */
+    private \Magento\Framework\View\DesignInterface $design;
+
+    /**
      * SetCvsLocationPlugin constructor.
      * @param \Eguana\GWLogistics\Api\QuoteCvsLocationRepositoryInterface $quoteCvsLocationRepository
      * @param \Magento\Quote\Api\Data\AddressExtensionFactory $addressExtensionFactory
@@ -35,11 +42,13 @@ class ShippingAddressManagementPlugin
     public function __construct(
         \Eguana\GWLogistics\Api\QuoteCvsLocationRepositoryInterface $quoteCvsLocationRepository,
         \Magento\Quote\Api\Data\AddressExtensionFactory $addressExtensionFactory,
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\View\DesignInterface $design
     ) {
         $this->quoteCvsLocationRepository = $quoteCvsLocationRepository;
         $this->addressExtensionFactory = $addressExtensionFactory;
         $this->logger = $logger;
+        $this->design = $design;
     }
 
     /**
@@ -49,18 +58,21 @@ class ShippingAddressManagementPlugin
      */
     public function afterGet(\Magento\Quote\Model\ShippingAddressManagementInterface $subject, \Magento\Quote\Api\Data\AddressInterface $result, $cartId)
     {
-        if ($result->getShippingMethod() == 'gwlogistics_CVS') {
-            if ($result->getData('cvs_location_id')) {
-                $extensionAttributes = $result->getExtensionAttributes();
-                try {
-                    $extensionAttributes->setCvsLocationId($result->getData('cvs_location_id'));
-                } catch (\Exception $e) {
-                    $this->logger->error("Error when set CVS Location Id: " . $e->getMessage());
-                    throw new LocalizedException(__('Cannot find the CVS store location. Please try to choose CVS store again if it still error, please contact our CS Center'));
+        if ($this->design->getDesignTheme()->getThemePath() != self::NEW_THEME_TW_LNG_PATH) {
+            if ($result->getShippingMethod() == 'gwlogistics_CVS') {
+                if ($result->getData('cvs_location_id')) {
+                    $extensionAttributes = $result->getExtensionAttributes();
+                    try {
+                        $extensionAttributes->setCvsLocationId($result->getData('cvs_location_id'));
+                    } catch (\Exception $e) {
+                        $this->logger->error("Error when set CVS Location Id: " . $e->getMessage());
+                        throw new LocalizedException(__('Cannot find the CVS store location. Please try to choose CVS store again if it still error, please contact our CS Center'));
+                    }
+                    $result->setExtensionAttributes($extensionAttributes);
                 }
-                $result->setExtensionAttributes($extensionAttributes);
             }
         }
+
         return $result;
     }
 
@@ -72,21 +84,23 @@ class ShippingAddressManagementPlugin
      */
     public function beforeAssign(\Magento\Quote\Model\ShippingAddressManagementInterface $subject, $cartId, AddressInterface $address)
     {
-        if ($address->getData('limit_carrier') === 'gwlogistics') {
-            $extensionAttributes = $address->getExtensionAttributes();
-            try {
-                $cvsLocation = $this->quoteCvsLocationRepository->getByQuoteId($cartId);
-                if ($cvsLocation->getLocationId()) {
-                    $extensionAttributes->setCvsLocationId($cvsLocation->getLocationId());
-                    $address->setCvsLocationId($extensionAttributes->getCvsLocationId());
-                } else {
+        if ($this->design->getDesignTheme()->getThemePath() != self::NEW_THEME_TW_LNG_PATH) {
+            if ($address->getData('limit_carrier') === 'gwlogistics') {
+                $extensionAttributes = $address->getExtensionAttributes();
+                try {
+                    $cvsLocation = $this->quoteCvsLocationRepository->getByQuoteId($cartId);
+                    if ($cvsLocation->getLocationId()) {
+                        $extensionAttributes->setCvsLocationId($cvsLocation->getLocationId());
+                        $address->setCvsLocationId($extensionAttributes->getCvsLocationId());
+                    } else {
+                        throw new LocalizedException(__('Cannot find the CVS store location. Please try to choose CVS store again if it still error, please contact our CS Center'));
+                    }
+                } catch (\Exception $e) {
+                    $this->logger->error("Error when choose CVS Location Id: " . $e->getMessage());
                     throw new LocalizedException(__('Cannot find the CVS store location. Please try to choose CVS store again if it still error, please contact our CS Center'));
                 }
-            } catch (\Exception $e) {
-                $this->logger->error("Error when choose CVS Location Id: " . $e->getMessage());
-                throw new LocalizedException(__('Cannot find the CVS store location. Please try to choose CVS store again if it still error, please contact our CS Center'));
+                $address->setExtensionAttributes($extensionAttributes);
             }
-            $address->setExtensionAttributes($extensionAttributes);
         }
         return [$cartId, $address];
     }
