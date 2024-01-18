9e5463762688008857e2e389651205e2b5999be4
Minh Manh <minh.manh@amorepacific.com>
2024-01-18 13:53:24 +0700

    [TWLSAPT-420]remove firstname,lastname section and add default firstname,lastname,telephone for cvs

--- app/code/Eguana/GWLogistics/Model/Ui/ConfigProvider.php
+++ app/code/Eguana/GWLogistics/Model/Ui/ConfigProvider.php
@@ -10,18 +10,32 @@ declare(strict_types=1);
 namespace Eguana\GWLogistics\Model\Ui;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Customer\Model\Session;
 
 class ConfigProvider implements ConfigProviderInterface
 {
     const CODE = 'gwlogistics';
+
     /**
      * @var \Eguana\GWLogistics\Helper\Data
      */
     private $helper;
 
-    public function __construct(\Eguana\GWLogistics\Helper\Data $helper)
-    {
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    /**
+     * @param \Eguana\GWLogistics\Helper\Data $helper
+     * @param Session $customerSession
+     */
+    public function __construct(
+        \Eguana\GWLogistics\Helper\Data $helper,
+        Session $customerSession
+    ) {
         $this->helper = $helper;
+        $this->customerSession = $customerSession;
     }
 
     public function getConfig()
@@ -30,11 +44,25 @@ class ConfigProvider implements ConfigProviderInterface
         if (!$isActive) {
             return [];
         }
+        $firstName = '';
+        $lastName = '';
+        $mobileNumber = '';
+        if ($this->customerSession->isLoggedIn()) {
+            $customer = $this->customerSession->getCustomer();
+            $firstName = $customer->getFirstname();
+            $lastName = $customer->getLastname();
+            $mobileNumber = $customer->getMobileNumber();
+        }
+
+
         return [
                 self::CODE => [
                     'isActive' => $isActive,
                     'shipping_message' => $this->helper->getCarrierShippingMessage(),
-                    'guest_cvs_shipping_method_enabled' => $this->helper->isGuestCVSShippingMethodEnabled()
+                    'guest_cvs_shipping_method_enabled' => $this->helper->isGuestCVSShippingMethodEnabled(),
+                    'cvs_first_name' => $firstName,
+                    'cvs_last_name' => $lastName,
+                    'mobile_number' => $mobileNumber
                 ],
         ];
     }
