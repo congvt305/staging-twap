bf9d7a0ae7285f672ec6a585dcbece40da7b1675
Minh Manh <minh.manh@amorepacific.com>
2024-01-25 14:52:44 +0700

    [BRANDSR-8081]fix error cannot zoom in zoom out for store list in mobile for TW LNG

--- app/design/frontend/AmorePacific/sapt/Eguana_StoreLocator/web/js/store-locator.js
+++ app/design/frontend/AmorePacific/sapt/Eguana_StoreLocator/web/js/store-locator.js
@@ -2,76 +2,71 @@ define([
 	'jquery',
 	'map-viewer'
 ], function ($) {
-	return function (config) {
-		$('.store-list').on('click', function () {
-			let storeInfo;
-			storeInfo = $(this).children('.store-info');
-			if (!$(this).hasClass('selected-store')) {
-				$(this).closest('.stores-listing-ul').children('li').each(function () {
-					let store = $(this).children('.store-info');
-					$(this).removeClass('selected-store');
-					store.children('.store-secondary-info').slideUp();
-				});
-				$(this).closest('.store-list').addClass('selected-store');
-				storeInfo.children('.store-secondary-info').slideDown();
-			}else {
-				if($(window).width() <= 991){
-					$(this).removeClass('selected-store');
-					$(this).find('.inner-store').slideUp(300, "easeOutSine");
-				}
+    return function (config) {
+        $('.store-list').on('click', function () {
+            let storeInfo;
+            storeInfo = $(this).children('.store-info');
+            if (!$(this).hasClass('selected-store')) {
+                $(this).closest('.stores-listing-ul').children('li').each(function () {
+                    let store = $(this).children('.store-info');
+                    $(this).removeClass('selected-store');
+                    store.children('.store-secondary-info').slideUp();
+                });
+                $(this).closest('.store-list').addClass('selected-store');
+                storeInfo.children('.store-secondary-info').slideDown();
+            }
+            appendTimer = setInterval(appendMapTimer, 100);
+        });
 
-			}
-			appendTimer = setInterval(appendMapTimer, 100);
-		});
-
-		/**
-		 * Reset interval time
-		 */
-		function appendMapTimer() {
-			clearInterval(appendTimer);
-			$(".store-list.selected-store .inner-store").append(htmlMapElement);
-		}
-		let windowSize = $(window).width();
-		let htmlMapElement, appendTimer;
-		function laodMobileView(x) {
-			if (x.matches) {
-				let mapHtmlParent = $(".stores-map").parent();
-				htmlMapElement = mapHtmlParent.children();
-				$("#store_map_viewer").css({"max-height":"600px"});
-				$('li.store-list').children('.inner-store').first().append(htmlMapElement);
-				$('.inner-store:not(:first-of-type)').css('display', 'block');
-				$('.store-list:first-of-type').addClass('current-li' , 300, "easeOutSine" );
-				$('.store-list:first-of-type').addClass('selected-store', 300 , "easeOutSine" );
-				$('li.store-list').on('click', function() {
-					if ($(this).hasClass('current-li')){
-						$(this).removeClass('current-li',300, "easeOutSine" );
-						$(this).find('.inner-store').slideUp(300, "easeOutSine");
-					} else {
-						$(".current-li").not(this).removeClass("current-li",300, "easeOutSine").find('.store-info').find('.inner-store').slideUp(300, "easeOutSine");
-						$(this).toggleClass('current-li',300, "easeOutSine" ).find('.store-info').find('.inner-store').slideToggle(300, "easeOutSine");
-						$(this).find('.inner-store').slideDown(300, "easeOutSine");
-					}
-				});
-			} else {
-				$( document ).ready(function() {
-					if($('li.store-list.selected-store .inner-store .stores-map').length) {
-						htmlMapElement = $('li.store-list.selected-store .inner-store').children();
-						$('.desktop-map_container .stores-content').append(htmlMapElement);
-					}else {
-						$('li.store-list').first().trigger('click');
-					}
-				});
-			}
-		}
-		if (config.locations) {
-			multi_map_initialize(config.locations, config.zoom, config.viewStore, config.markerImages, config.north, config.south, config.west, config.east);
-		}
-		pushmarker(0);
-		$( document ).ready(function() {
-			if ($('#current-lat').val().length != 0) {
-				$('.map-open').attr('state-dir','1');
-			}
-		});
+        /**
+         * Reset interval time
+         */
+        function appendMapTimer() {
+            clearInterval(appendTimer);
+            $(".store-list.selected-store .inner-store").append(htmlMapElement);
+        }
+        let windowSize = $(window).width();
+        let htmlMapElement, appendTimer;
+        function laodMobileView(x) {
+            if (x.matches) {
+                let mapHtmlParent = $(".stores-map").parent();
+                htmlMapElement = mapHtmlParent.children();
+                $("#store_map_viewer").css({"max-height":"600px"});
+                $('li.store-list').children('.inner-store').first().append(htmlMapElement);
+                $('.inner-store:not(:first-of-type)').css('display', 'block');
+                $('.store-list:first-of-type').addClass('current-li' , 300, "easeOutSine" );
+                $('.store-list:first-of-type').addClass('selected-store', 300 , "easeOutSine" );
+                $('.store-info').on('click', function() {
+                    var liTag = $(this).parent('li.store-list');
+                    if (liTag.hasClass('current-li')){
+                        liTag.removeClass('current-li',300, "easeOutSine" );
+                        liTag.find('.inner-store').slideUp(300, "easeOutSine");
+                    } else {
+                        $(".current-li").not(liTag).removeClass("current-li",300, "easeOutSine").find('.store-info').find('.inner-store').slideUp(300, "easeOutSine");
+                        liTag.toggleClass('current-li',300, "easeOutSine" ).find('.store-info').find('.inner-store').slideToggle(300, "easeOutSine");
+                        liTag.find('.inner-store').slideDown(300, "easeOutSine");
+                    }
+                });
+            } else {
+                $( document ).ready(function() {
+                    if($('li.store-list.selected-store .inner-store .stores-map').length) {
+                        htmlMapElement = $('li.store-list.selected-store .inner-store').children();
+                        $('.desktop-map_container .stores-content').append(htmlMapElement);
+                    }else {
+                        $('li.store-list').first().trigger('click');
+                    }
+                });
+            }
+        }
+        if (config.locations) {
+            multi_map_initialize(config.locations, config.zoom, config.viewStore, config.markerImages, config.north, config.south, config.west, config.east);
+        }
+        pushmarker(0);
+        $( document ).ready(function() {
+            if ($('#current-lat').val().length != 0) {
+                $('.map-open').attr('state-dir','1');
+            }
+        });
 
 		/**
 		 * set direction for source to destination
